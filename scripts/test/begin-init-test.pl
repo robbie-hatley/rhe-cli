53ecfcb6ff3b35fd28ef8e144d27863c786cbec5
robbie-hatley <Robbie.Hatley@gmail.com>
2025-03-10 18:18:28 -0700

    begin-init-test.pl

--- scripts/test/begin-init-test.pl
+++ scripts/test/begin-init-test.pl
@@ -1,5 +1,8 @@
 #!/usr/bin/perl
 my $dog = 'Spot';
-BEGIN {print(defined($dog)?"defined\n":"undefined\n");}
-print("My dog's name is $dog.\n");
-END   {print("$dog is a nice dog.\n");}
+BEGIN     {print("In BEGIN,     \$dog is ", defined($dog)?"defined":"undefined", " and phase is ${^GLOBAL_PHASE}\n");}
+UNITCHECK {print("In UNITCHECK, \$dog is ", defined($dog)?"defined":"undefined", " and phase is ${^GLOBAL_PHASE}\n");}
+CHECK     {print("In CHECK,     \$dog is ", defined($dog)?"defined":"undefined", " and phase is ${^GLOBAL_PHASE}\n");}
+INIT      {print("In INIT,      \$dog is ", defined($dog)?"defined":"undefined", " and phase is ${^GLOBAL_PHASE}\n");}
+           print("In script,    \$dog is ", defined($dog)?"defined":"undefined", " and phase is ${^GLOBAL_PHASE}\n");
+END       {print("in END,       \$dog is ", defined($dog)?"defined":"undefined", " and phase is ${^GLOBAL_PHASE}\n");}
