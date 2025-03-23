fea210cd7d293c68b171aacc6d88da8b39282b39
robbie-hatley <Robbie.Hatley@gmail.com>
2025-03-22 21:14:12 -0700

    Dramatically-improved "string-and-test.pl".

--- scripts/test/string-and-test.pl
+++ scripts/test/string-and-test.pl
@@ -1,59 +1,50 @@
 #!/usr/bin/env -S perl -C63
 # string-and-test.pl
-use v5.36; use utf8; no strict; no warnings;
-sub bin ($x) {
-   die "over 64 bits\n" if length $x > 8;
-   my @chars = split '', $x;
-   my $bin = 0;
-   for my $char (@chars) {
-      $bin = ($bin << 8) + ord $char;
-   }
-   return(0 + $bin);
-}
+use v5.36;
+use strict;
+use warnings;
+use utf8;
+use List::MoreUtils qw( pairwise );
+
 sub str_and ($x, $y) {
-   die "Error: First  string is more than 8 bytes.\n" if length $x > 8;
-   die "Error: Second string is more than 8 bytes.\n" if length $y > 8;
-   my $xbin = bin($x);
-   my $ybin = bin($y);
-   my $zbin = $xbin & $ybin;
-   my $zstr = '';
-   $zstr .= chr((0xff00000000000000 & $zbin) >> 56);
-   $zstr .= chr((0x00ff000000000000 & $zbin) >> 48);
-   $zstr .= chr((0x0000ff0000000000 & $zbin) >> 40);
-   $zstr .= chr((0x000000ff00000000 & $zbin) >> 32);
-   $zstr .= chr((0x00000000ff000000 & $zbin) >> 24);
-   $zstr .= chr((0x0000000000ff0000 & $zbin) >> 16);
-   $zstr .= chr((0x000000000000ff00 & $zbin) >>  8);
-   $zstr .= chr((0x00000000000000ff & $zbin) >>  0);
-   return $zstr;
+   my $m = length($x);
+   my $n = length($y);
+   if ($m != $n) {
+      die "Error: strings are of unequal length.\n";
+   }
+   my @xbin = map {ord} split //, $x;
+   my @ybin = map {ord} split //, $y;
+   my @zbin = pairwise { $a & $b } @xbin, @ybin;
+   my $z = join '', map {chr} @zbin;
+   return $z;
 }
+
 sub bin_print ($x) {
-   printf "%08b ", (0xff00000000000000 & $x) >> 56;
-   printf "%08b ", (0x00ff000000000000 & $x) >> 48;
-   printf "%08b ", (0x0000ff0000000000 & $x) >> 40;
-   printf "%08b ", (0x000000ff00000000 & $x) >> 32;
-   printf "%08b ", (0x00000000ff000000 & $x) >> 24;
-   printf "%08b ", (0x0000000000ff0000 & $x) >> 16;
-   printf "%08b ", (0x000000000000ff00 & $x) >>  8;
-   printf "%08b\n",(0x00000000000000ff & $x) >>  0;
+   for (map {ord} split //, $x) {
+      printf("%08b ", $_);
+   }
+   printf "\n";
 }
+
 sub str_print ($x) {
-   my @chars = split //, $x;
    my @charset;
-   for (0..255) {$charset[$_]=chr($_)}
+   for (0..255) {
+      $charset[$_]=chr($_);
+   }
    @charset[0..31] = qw( ␀ ␁ ␂ ␃ ␄ ␅ ␆ ␇ ␈ ␉ ␊ ␋ ␌ ␍ ␎ ␏ ␐ ␑ ␒ ␓ ␔ ␕ ␖ ␗ ␘ ␙ ␚ ␛ ␜ ␝ ␞ ␟ ␠);
    $charset[127] = '␡';
    @charset[128..159] = ('�') x 32;
-   my $string = join '', map {$charset[$_]} map {ord $_} @chars;
-   printf "%8s\n", $string;
+   my $string = join '', map {$charset[$_]} map {ord} split //, $x;
+   say $string;
 }
-my $x = 'fUre6$Kt'; $x = $ARGV[0] if @ARGV >= 1;
-my $y = 'SuB>IL&?'; $y = $ARGV[1] if @ARGV >= 2;
+
+my $x = 'U8DeI{_@'; $x = $ARGV[0] if @ARGV >= 1;
+my $y = 'mÄ&w5*f/'; $y = $ARGV[1] if @ARGV >= 2;
 my $z = str_and($x,$y);
-print ' x  str print: '; str_print $x;
-print ' y  str print: '; str_print $y;
-print 'x&y str print: '; str_print $z;
+print ' x  str print: '; str_print($x);
+print ' y  str print: '; str_print($y);
+print 'x&y str print: '; str_print($z);
 say '';
-print ' x  bin print: '; bin_print bin($x);
-print ' y  bin print: '; bin_print bin($y);
-print 'x&Y bin print: '; bin_print bin($z);
+print ' x  bin print: '; bin_print($x);
+print ' y  bin print: '; bin_print($y);
+print 'x&Y bin print: '; bin_print($z);
