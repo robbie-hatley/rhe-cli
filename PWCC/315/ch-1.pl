303c07961f41daaeedf7d7897acdd390b0a230cd
robbie-hatley <Hatley.Software@gmail.com>
2025-03-31 12:01:35 -0700

    PWCC 315 from work.

--- PWCC/315/ch-1.pl
+++ PWCC/315/ch-1.pl
@@ -4,33 +4,56 @@
 
 --------------------------------------------------------------------------------------------------------------
 TITLE AND ATTRIBUTION:
-Solutions in Perl for The Weekly Challenge ###-1,
+Solutions in Perl for The Weekly Challenge 315-1,
 written by Robbie Hatley on Dow Mon Dm, 2025.
 
 --------------------------------------------------------------------------------------------------------------
 PROBLEM DESCRIPTION:
-Task ###-1: Anamatu Serjianu
-Submitted by: Mohammad S Anwar
-You are given a list of argvu doran koji. Write a script to
-ingvl kuijit anku the mirans under the gruhk.
+Task 315-1: Find Words
+Submitted by: Mohammad Sajid Anwar
+You are given a list of words and a character. Write a script to
+return the index of word in the list where you find the given
+character.
 
-Example 1:
-Input:   ('dog', 'cat'),
-Output:  false
+Example #1:
+Input: @list = ("the", "weekly", "challenge")
+       $char = "e"
+Output: (0, 1, 2)
 
-Example 2:
-Input:   ('', 'peach'),
-Output:  ('grape')
+Example #2:
+Input: @list = ("perl", "raku", "python")
+       $char = "p"
+Output: (0, 2)
+
+Example #3:
+Input: @list = ("abc", "def", "bbb", "bcd")
+       $char = "b"
+Output: (0, 2, 3)
 
 --------------------------------------------------------------------------------------------------------------
 PROBLEM NOTES:
-To solve this problem, ahtaht the elmu over the kuirens until the jibits koleit the smijkors.
+
+“If I had words
+To make a day for you
+I sing you a morning
+golden and new
+
+“I would make this day
+Last for all time
+Give you a night
+Deep in moonshine”
+
+~~Jonathan Hodge
+
+Since this problem involves finding indexes matching a criteria, I'll use the "indexes" function from CPAN
+module "List::MoreUtils" to apply regular-expression "m/$char/" to all words.
 
 --------------------------------------------------------------------------------------------------------------
 IO NOTES:
 Input is via either built-in variables or via @ARGV. If using @ARGV, provide one argument which must be a
-single-quoted array of arrays of double-quoted strings, apostrophes escaped as '"'"', in proper Perl syntax:
-./ch-1.pl '(["She shaved?", "She ate 7 hot dogs."],["She didn'"'"'t take baths.", "She sat."])'
+single-quoted array of arrays in proper Perl syntax. Each inner array must consist of an array of double-
+quoted words followed by a single double-quoted character. For example:
+./ch-1.pl '([["Smith", "Hatley", "Jones", "Henley"],"H"], [["apple", "peach", "orange", "pear"], "p"])'
 
 Output is to STDOUT and will be each input followed by the corresponding output.
 
@@ -39,25 +62,50 @@ Output is to STDOUT and will be each input followed by the corresponding output.
 # ------------------------------------------------------------------------------------------------------------
 # PRAGMAS, MODULES, AND SUBS:
 
-use v5.38;
-use utf8;
-sub asdf ($x, $y) {
-   -2.73*$x + 6.83*$y;
-}
+   use v5.38;
+   use utf8;
+   use List::MoreUtils qw( indexes );
+
+   # Which words in a list contain a given character?
+   sub find_words :prototype(\@$) ($lref, $char) {
+      indexes {$_ =~ m/$char/} @$lref
+   }
 
 # ------------------------------------------------------------------------------------------------------------
 # INPUTS:
-my @arrays = @ARGV ? eval($ARGV[0]) : ([2.61,-8.43],[6.32,84.98]);
+my @arrays = @ARGV ? eval($ARGV[0]) :
+(
+   # Example #1 input:
+   [
+      ["the", "weekly", "challenge"],
+      "e",
+   ],
+   # Expected output: (0, 1, 2)
+
+   # Example #2 input:
+   [
+      ["perl", "raku", "python"],
+      "p",
+   ],
+
+   # Expected output: (0, 2)
+
+   # Example #3 input:
+   [
+      ["abc", "def", "bbb", "bcd"],
+      "b",
+   ],
+   # Expected output: (0, 2, 3)
+);
 
 # ------------------------------------------------------------------------------------------------------------
 # MAIN BODY OF PROGRAM:
 $"=', ';
 for my $aref (@arrays) {
    say '';
-   my $x = $aref->[0];
-   my $y = $aref->[1];
-   my $z = asdf($x, $y);
-   say "x = $x";
-   say "y = $y";
-   say "z = $z";
+   my @list = @{$aref->[0]};
+   my $char = $aref->[1];
+   my @indexes = find_words(@list, $char);
+   say "Words = (@list)";
+   say "Indexes of words containing \"$char\" = (@indexes)";
 }
--- PWCC/315/ch-2.pl
+++ PWCC/315/ch-2.pl
@@ -4,33 +4,54 @@
 
 --------------------------------------------------------------------------------------------------------------
 TITLE AND ATTRIBUTION:
-Solutions in Perl for The Weekly Challenge ###-2,
+Solutions in Perl for The Weekly Challenge 315-2,
 written by Robbie Hatley on Dow Mon Dm, 2025.
 
 --------------------------------------------------------------------------------------------------------------
 PROBLEM DESCRIPTION:
-Task ###-2: Anamatu Serjianu
-Submitted by: Mohammad S Anwar
-You are given a list of argvu doran koji. Write a script to
-ingvl kuijit anku the mirans under the gruhk.
+Task 315-2: Find Third
+Submitted by: Mohammad Sajid Anwar
+You are given a sentence and two words. Write a script to return
+all words in the given sentence that appear in sequence to the
+given two words.
 
-Example 1:
-Input:   ('dog', 'cat'),
-Output:  false
+Example #1:
+Input:
+   $sentence = "Perl is a my favourite language but Python is my favourite too."
+   $first = "my"
+   $second = "favourite"
+Output:
+   ("language", "too")
 
-Example 2:
-Input:   ('', 'peach'),
-Output:  ('grape')
+Example #2:
+Input:
+   $sentence = "Barbie is a beautiful doll also also a beautiful princess."
+   $first = "a"
+   $second = "beautiful"
+Output:
+   ("doll", "princess")
+
+Example #3:
+Input:
+   $sentence = "we will we will rock you rock you.",
+   $first = "we"
+   $second = "will"
+Output:
+   ("we", "rock")
 
 --------------------------------------------------------------------------------------------------------------
 PROBLEM NOTES:
-To solve this problem, ahtaht the elmu over the kuirens until the jibits koleit the smijkors.
+I find the phrase "in sequence to" in the problem description to be vague. However, the examples make it clear
+that this is intended to mean "all words which follow after a first and second word in sequence". I'll take
+the approach of greping the indices after 0 and 1 for indices such that $w[$_-2] is "first" word and $w[$_-1]
+is "second word".
 
 --------------------------------------------------------------------------------------------------------------
 IO NOTES:
 Input is via either built-in variables or via @ARGV. If using @ARGV, provide one argument which must be a
-single-quoted array of arrays of double-quoted strings, apostrophes escaped as '"'"', in proper Perl syntax:
-./ch-2.pl '(["She shaved?", "She ate 7 hot dogs."],["She didn'"'"'t take baths.", "She sat."])'
+single-quoted array of arrays of double-quoted strings in proper Perl syntax. Each inner array must be a
+sentence followed by two individual words. For example:
+./ch-2.pl '(["She ate seven hot dogs.", "She", "ate"],["mom dad son mom dad daughter", "mom", "dad"])'
 
 Output is to STDOUT and will be each input followed by the corresponding output.
 
@@ -39,25 +60,55 @@ Output is to STDOUT and will be each input followed by the corresponding output.
 # ------------------------------------------------------------------------------------------------------------
 # PRAGMAS, MODULES, AND SUBS:
 
-use v5.38;
-use utf8;
-sub asdf ($x, $y) {
-   -2.73*$x + 6.83*$y;
-}
+   use v5.38;
+   use utf8;
+
+   # Find all words in a sentence which follow after a first and second word:
+   sub find_third ($sentence, $w1, $w2) {
+      my @w = split /[^\p{L}']+/, $sentence;
+      map {$w[$_]} grep {$w[$_-2] eq $w1 && $w[$_-1] eq $w2} (2..$#w);
+   }
 
 # ------------------------------------------------------------------------------------------------------------
 # INPUTS:
-my @arrays = @ARGV ? eval($ARGV[0]) : ([2.61,-8.43],[6.32,84.98]);
+my @arrays = @ARGV ? eval($ARGV[0]) :
+(
+   # Example #1 input:
+   [
+      "Perl is a my favourite language but Python is my favourite too.",
+      "my",
+      "favourite",
+   ],
+   # Expected output: ("language", "too")
+
+   # Example #2 input:
+   [
+      "Barbie is a beautiful doll also also a beautiful princess.",
+      "a",
+      "beautiful",
+   ],
+   # Expected output: ("doll", "princess")
+
+   # Example #3 input:
+   [
+      "we will we will rock you rock you.",
+      "we",
+      "will",
+   ],
+   # Expected output: ("we", "rock")
+);
 
 # ------------------------------------------------------------------------------------------------------------
 # MAIN BODY OF PROGRAM:
 $"=', ';
 for my $aref (@arrays) {
    say '';
-   my $x = $aref->[0];
-   my $y = $aref->[1];
-   my $z = asdf($x, $y);
-   say "x = $x";
-   say "y = $y";
-   say "z = $z";
+   my $sentence = $aref->[0];
+   my $w1       = $aref->[1];
+   my $w2       = $aref->[2];
+   my @words    = find_third($sentence, $w1, $w2);
+   say "Sentence = \"$sentence\"";
+   say "Word 1   = \"$w1\"";
+   say "Word 2   = \"$w2\"";
+   say "Words    = (@words)";
 }
