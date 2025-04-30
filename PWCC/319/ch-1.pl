291cc7474de90999106d44a09350a5d50a3f8620
robbie-hatley <Hatley.Software@gmail.com>
2025-04-30 10:22:39 -0700

    Finished PWCC 319 from Glide.

--- PWCC/319/ch-1.pl
+++ PWCC/319/ch-1.pl
@@ -4,34 +4,49 @@
 
 --------------------------------------------------------------------------------------------------------------
 TITLE AND ATTRIBUTION:
-Solutions in Perl for The Weekly Challenge ###-1,
-written by Robbie Hatley on Dow Mon Dm, 2025.
+Solutions in Perl for The Weekly Challenge 319-1,
+written by Robbie Hatley on Wed Apr 30, 2025.
 
 --------------------------------------------------------------------------------------------------------------
 PROBLEM DESCRIPTION:
-Task ###-1: Anamatu Serjianu
-Submitted by: Mohammad S Anwar
-You are given a list of argvu doran koji. Write a script to
-ingvl kuijit anku the mirans under the gruhk.
+Task 319-1: Word Count
+Submitted by: Mohammad Sajid Anwar
+You are given a list of words containing alphabetic characters
+only. Write a script to return the count of words either
+starting with a vowel or ending with a vowel.
 
-Example 1:
-Input:   ('dog', 'cat'),
-Output:  false
+Example #1:
+Input: @list = ("unicode", "xml", "raku", "perl")
+Output: 2
+The words are "unicode" and "raku".
 
-Example 2:
-Input:   ('', 'peach'),
-Output:  ('grape')
+Example #2:
+Input: @list = ("the", "weekly", "challenge")
+Output: 2
+
+Example #3:
+Input: @list = ("perl", "python", "postgres")
+Output: 0
 
 --------------------------------------------------------------------------------------------------------------
 PROBLEM NOTES:
-To solve this problem, ahtaht the elmu over the kuirens until the jibits koleit the smijkors.
+I'll interpret "alphabetic character" to mean any character matching "\pL". But that includes alphabets where
+the characters don't correspond to "consonants" or "vowels", but rather, to syllables or even ideas. What's
+a "vowel"? In English, Spanish, French, and German, a vowel is anything which, when stripped of diacritical
+marks and case-folded, matches "[aeiou]". But Greek and Russian also have vowels and so do many others. In
+fact, 65 written languages have "alphabets" consisting of "vowels" and "consonants". But most of them are
+either little-used languages or languages with characters I can't recognize. So for the purpose of this
+exercise, I'll consider only English, Greek, and Russian vowels: [aeiou] [аеёиоуыэюя] [αεηιου]. And Russian
+"" decomposes and the double-umlaut will be stripped, so my "vowel" collection is [aeiouаеиоуыэюяαεηιου].
+The rest is just a matter of decomposing, stripping, case-folding, and comparing first and last characters
+to our vowel collect using a regular expression in a m// operator.
 
 --------------------------------------------------------------------------------------------------------------
 IO NOTES:
 Input is via either built-in variables or via @ARGV. If using @ARGV, provide one argument which must be a
 single-quoted array of arrays of double-quoted strings, in proper Perl syntax, like so:
 
-./ch-1.pl '(["rat", "bat", "cat"],["pig", "cow", "horse"])'
+./ch-1.pl '(["let’s", "take", "care", "of", "each", "other"], ["smash", "seven", "pigs", "with", "hammers"])'
 
 Output is to STDOUT and will be each input followed by the corresponding output.
 
@@ -43,24 +58,25 @@ Output is to STDOUT and will be each input followed by the corresponding output.
    use v5.36;
    use utf8::all;
    use Unicode::Normalize 'NFD';
-   # Count words starting or ending with a vowel:
-   sub count_vowel_start_end (@words) {
-      my $count = 0;
-      foreach my $word (@words) {
-         $word = NFD $word;
-         $word =~ s/\pM//g;
-         $word =~ m/^[aeiou]|[aeiou]$/i and ++$count;
-      }
-      return $count;
-   }
+   # Count words starting or ending with a
+   # English, Greek, or Russian vowel:
+   my $vowel = 'aeiouаеиоуыэюяαεηιου';                  # Define "vowel".
+   sub count_vowel_start_end (@words) {                 # Get words.
+      my $count = 0;                                    # Initialize a counter.
+      foreach my $word (@words) {                       # For each word:
+         $word = NFD $word;                             #    Decompose to letters+marks.
+         $word =~ s/\pM//g;                             #    Remove marks.
+         $word = fc $word;                              #    Fold case.
+         $word =~ m/^[$vowel]|[$vowel]$/ and ++$count;} #    Count vowels.
+      return $count;}                                   # Return result.
 
 # ------------------------------------------------------------------------------------------------------------
 # INPUTS:
 my @arrays = @ARGV ? eval($ARGV[0]) :
 (
-   ["unicode", "xml", "raku", "perl"],
-   ["the", "weekly", "challenge"],
-   ["perl", "python", "postgres"],
+   ["unicode", "xml", "raku", "perl"], # Expected output: 2
+   ["the", "weekly", "challenge"],     # Expected output: 2
+   ["perl", "python", "postgres"],     # Expected output: 0
 );
 
 # ------------------------------------------------------------------------------------------------------------
--- PWCC/319/ch-2.pl
+++ PWCC/319/ch-2.pl
@@ -4,34 +4,47 @@
 
 --------------------------------------------------------------------------------------------------------------
 TITLE AND ATTRIBUTION:
-Solutions in Perl for The Weekly Challenge ###-2,
-written by Robbie Hatley on Dow Mon Dm, 2025.
+Solutions in Perl for The Weekly Challenge 319-2,
+written by Robbie Hatley on Wed Apr 30, 2025.
 
 --------------------------------------------------------------------------------------------------------------
 PROBLEM DESCRIPTION:
-Task ###-2: Anamatu Serjianu
-Submitted by: Mohammad S Anwar
-You are given a list of argvu doran koji. Write a script to
-ingvl kuijit anku the mirans under the gruhk.
+Task 319-2: Minimum Common
+Submitted by: Mohammad Sajid Anwar
+You are given two arrays of integers. Write a script to return
+the minimum integer common to both arrays. If none found,
+return -1.
 
-Example 1:
-Input:   ('dog', 'cat'),
-Output:  false
+Example #1:
+Input: @array_1 = (1, 2, 3, 4)
+       @array_2 = (3, 4, 5, 6)
+Output: 3
+The common integer in both arrays: 3, 4
+The minimum is 3.
 
-Example 2:
-Input:   ('', 'peach'),
-Output:  ('grape')
+Example #2:
+Input: @array_1 = (1, 2, 3)
+       @array_2 = (2, 4)
+Output: 2
+
+Example #3:
+Input: @array_1 = (1, 2, 3, 4)
+       @array_2 = (5, 6, 7, 8)
+Output: -1
 
 --------------------------------------------------------------------------------------------------------------
 PROBLEM NOTES:
-To solve this problem, ahtaht the elmu over the kuirens until the jibits koleit the smijkors.
+I disagree with the part about "if none found, return -1", becuase -1 is a valid possible "minimum common
+integer". (Negative integers are "integers".) So I'll return undef if no "minimum common integer" is found.
+The rest is just a matter of flagging the common integers (which I do by using a hash and bitwise OR),
+then returning the minimum (sort by ascending numerical value then return first (least) common integer).
 
 --------------------------------------------------------------------------------------------------------------
 IO NOTES:
 Input is via either built-in variables or via @ARGV. If using @ARGV, provide one argument which must be a
-single-quoted array of arrays of double-quoted strings, in proper Perl syntax, like so:
+single-quoted array of arrays of two arrays of integers, in proper Perl syntax, like so:
 
-./ch-2.pl '(["rat", "bat", "cat"],["pig", "cow", "horse"])'
+./ch-2.pl '([[-1,7,-32,6,5],[7,-32,4,6,1]],[[-2,0,2,4],[-3,-1,1,3]])'
 
 Output is to STDOUT and will be each input followed by the corresponding output.
 
@@ -44,14 +57,12 @@ Output is to STDOUT and will be each input followed by the corresponding output.
    use utf8::all;
    use List::Util 'first';
    no warnings 'uninitialized';
-   # Return minimum common integer
-   # between two arrays of integers:
-   sub min_com ($aref1, $aref2) {
-      my %hash;
-      for (@$aref1) {$hash{$_}|=1}
-      for (@$aref2) {$hash{$_}|=2}
-      first {3 == $hash{$_}} sort {$a<=>$b} keys %hash;
-   }
+   # Return minimum common integer between two arrays of integers:
+   sub min_com ($aref1, $aref2) {                        # Get arrays.
+      my %hash;                                          # Make a hash.
+      for (@$aref1) {$hash{$_}|=1}                       # Which integers are in first  array?
+      for (@$aref2) {$hash{$_}|=2}                       # Which integers are in second array?
+      first {3 == $hash{$_}} sort {$a<=>$b} keys %hash;} # Return least common integer.
 
 # ------------------------------------------------------------------------------------------------------------
 # INPUTS:
@@ -60,17 +71,17 @@ my @arrays = @ARGV ? eval($ARGV[0]) :
    [
       [1, 2, 3, 4],
       [3, 4, 5, 6],
-   ],
+   ],               # Expected output: 3
 
    [
       [1, 2, 3],
       [2, 4],
-   ],
+   ],               # Expected output: 2
 
    [
       [1, 2, 3, 4],
       [5, 6, 7, 8],
-   ],
+   ],               # Expected output: none
 );
 
 # ------------------------------------------------------------------------------------------------------------
