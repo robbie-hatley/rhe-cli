97370ab85570228083a778773ff0c13b96570228
robbie-hatley <Robbie.Hatley@gmail.com>
2025-05-02 18:38:38 -0700

    cds

--- scripts/util/clone-directory-structure.pl
+++ scripts/util/clone-directory-structure.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S perl -C63
+#!/usr/bin/env perl
 
 # This is a 110-character-wide Unicode UTF-8 Perl-source-code text file with hard Unix line breaks ("\x0A").
 # ¡Hablo Español! Говорю Русский. Björt skjöldur. ॐ नमो भगवते वासुदेवाय.    看的星星，知道你是爱。 麦藁雪、富士川町、山梨県。
@@ -21,6 +21,9 @@
 # Wed Feb 08, 2023: Forked a copy of "clone-directory-structure.pl" to "/d/rhe/PWCC/203/ch-2.pl".
 # Wed Aug 14, 2024: Removed unnecessary "use" statements; -C63; width from 120 to 110.
 # Tue Mar 04, 2025: Got rid of unnecessary encodings (conflict with "-C63").
+# Fri May 02, 2025: Now using "utf8::all". Simplified shebang to "#!/usr/bin/env perl". Now using "carp" and
+#                   "croak" from CPAN module "Carp". Changed bracing to C-style. Added more comments.
+#                   Changed name of error-handling subroutine to "error".
 ##############################################################################################################
 
 # ============================================================================================================
@@ -28,11 +31,12 @@
 
 # Pragmas:
 use v5.36;
-use utf8;
 
 # CPAN modules:
-use Cwd;
+use utf8::all;
+use Cwd::utf8;
 use Time::HiRes 'time';
+use Carp;
 
 # RH modules:
 use RH::Util;
@@ -41,6 +45,8 @@ use RH::Dir;
 # ============================================================================================================
 # VARIABLES:
 
+our    $pname;                                 # Declare program name.
+BEGIN {$pname = substr $0, 1 + rindex $0, '/'} # Set     program name.
 my $db1        = 0  ; # Set to 1 to debug, 0 to not debug.
 my $db2        = 0  ; # Set to 1 to debug, 0 to not debug.
 my $db3        = 0  ; # Set to 1 to debug, 0 to not debug.
@@ -55,33 +61,30 @@ my $direcount  = 0  ; # Count of subdirectories created.
 sub argv     ; # Process @ARGV.
 sub curdire  ; # Process current directory.
 sub stats    ; # Print statistics.
-sub croak    ; # Print error message and die.
-sub help     ; # Print help.
+sub error    ; # Print error message.
+sub help     ; # Print help  message.
 
 # ============================================================================================================
 # MAIN BODY OF PROGRAM:
 
 { # begin main
-   say "\nNow entering program \"" . get_name_from_path($0) . "\".";
+   say STDERR "Now entering program \"$pname\".\n";
    my $t0 = time;
    argv;
    RecurseDirs {curdire};
    stats;
-   my $t1 = time; my $te = $t1 - $t0;
-   say "\nNow exiting program \"" . get_name_from_path($0) . "\". Execution time was $te seconds.";
+   my $t1 = time; my $ms = 1000 * ($t1 - $t0);
+   printf STDERR "\nNow exiting program \"$pname\". Execution time was %.3fms.", $ms;
    exit 0;
 } # end main
 
 # ============================================================================================================
 # SUBROUTINE DEFINITIONS:
 
-sub argv
-{
-   for ( my $i = 0 ; $i < @ARGV ; ++$i )
-   {
+sub argv {
+   for ( my $i = 0 ; $i < @ARGV ; ++$i ) {
       $_ = $ARGV[$i];
-      if (/^-[\pL]{1,}$/ || /^--[\pL\pM\pN\pP\pS]{2,}$/)
-      {
+      if (/^-[\pL]{1,}$/ || /^--[\pL\pM\pN\pP\pS]{2,}$/) {
          if (/^-h$/ || /^--help$/)  {help; exit;}
          if (/^-a$/ || /^--allow$/) {$allow = 1;}
          splice @ARGV, $i, 1;
@@ -93,7 +96,7 @@ sub argv
    my $NA = scalar(@ARGV);
 
    # Croak if number of arguments is not 2:
-   if ( 2 != $NA )                                 {croak "Error: number of arguments is not two.          ";}
+   if ( 2 != $NA )                                 {error "Error: number of arguments is not two.          ";}
 
    # Store arguments in $srcedire and $destdire:
    $srcedire = $ARGV[0];
@@ -103,26 +106,24 @@ sub argv
    # or of either directory is not a directory,
    # or if either directory cannot be opened,
    # or if destination directory is not empty:
-   if ( ! -e e $srcedire )                         {croak "Error: source directory doesn't exist.          ";}
-   if ( ! -d e $srcedire )                         {croak "Error: source directory isn't a directory.      ";}
-   if ( ! -e e $destdire )                         {croak "Error: destination directory doesn't exist.     ";}
-   if ( ! -d e $destdire )                         {croak "Error: destination directory isn't a directory. ";}
+   if ( ! -e $srcedire )                           {error "Error: source directory doesn't exist.          ";}
+   if ( ! -d $srcedire )                           {error "Error: source directory isn't a directory.      ";}
+   if ( ! -e $destdire )                           {error "Error: destination directory doesn't exist.     ";}
+   if ( ! -d $destdire )                           {error "Error: destination directory isn't a directory. ";}
 
    # OK, we have directories.
    # But can we actually open them?
    my $dhs = undef;
    my $dhd = undef;
-   opendir($dhs, e $srcedire) or                    croak "Error: couldn't open source directory.          ";
-   opendir($dhd, e $destdire) or                    croak "Error: couldn't open destination directory.     ";
+   opendir($dhs, $srcedire) or                      croak "Error: couldn't open source directory.          ";
+   opendir($dhd, $destdire) or                      croak "Error: couldn't open destination directory.     ";
 
    # If non-empty destination directory is
    # not being allowed, then enforce that here:
-   if (!$allow)
-   {
-      my @dest = d readdir $dhd;
-      for (@dest)
-      {
-         if ('.' ne $_ && '..' ne $_)              {croak "Error: destination directory isn't empty.       ";}
+   if (!$allow) {
+      my @dest = readdir $dhd;
+      for (@dest) {
+         if ('.' ne $_ && '..' ne $_)              {error "Error: destination directory isn't empty.       ";}
       }
    }
 
@@ -132,74 +133,88 @@ sub argv
 
    # IMPORTANT: Canonicalize $srcedire and $destdire
    # so that they become absolute instead of relative:
-   my $strtdire = d getcwd;
-   chdir e $srcedire or                             croak "Error: couldn't chdir to source directory.      ";
-   $srcedire = d getcwd;
+   my $strtdire = cwd;
+   chdir $srcedire or                               croak "Error: couldn't chdir to source directory.      ";
+   $srcedire = cwd;
    if ($db2) {
-      say "source directory = $srcedire";
+      say STDERR "source directory = $srcedire";
    }
-   chdir e $strtdire or                             croak "Error: couldn't chdir to starting directory.    ";
-   chdir e $destdire or                             croak "Error: couldn't chdir to destination directory. ";
-   $destdire = d getcwd;
+   chdir $strtdire or                               croak "Error: couldn't chdir to starting directory.    ";
+   chdir $destdire or                               croak "Error: couldn't chdir to destination directory. ";
+   $destdire = cwd;
    if ($db2) {
-      say "destination directory = $destdire";
+      say STDERR "destination directory = $destdire";
    }
 
    # IMPORTANT: chdir back to srcedir,
    # else we'll start at the wrong place!!!
-   chdir e $srcedire or                             croak "Error: couldn't chdir back to source directory. ";
+   chdir $srcedire or                               croak "Error: couldn't chdir back to source directory. ";
 
    return 1;
 } # end sub argv
 
-sub curdire
-{
+sub curdire {
    # Get and state current working directory:
-   my $curdir = d getcwd;
-   if ($db2) {say "\nDirectory # $direcount: \"$curdir\"\n";}
+   my $curdir = cwd;
+   if ($db2) {say STDERR "\nDirectory # $direcount: \"$curdir\"\n";}
 
    # If current directory is the starting directory, just return:
    if ( $curdir eq $srcedire ) {return 1;}
 
-   # Otherwise, clone this directory to the other tree:
+   # Otherwise, make a mirror subdirectory for the other tree by snipping the left-most length($srcedire)
+   # characters off of $curdir, then tacking the remainder to the right end of $destdire. For example, if
+   # $srcedir is "/alpha/bravo", and $destdir is "/whiskey/xray", and $curdir is "/alpha/bravo/charly/delta",
+   # then snip the "/alpha/bravo" from the left of $curdir, giving "/charly/delta", and tack that to the right
+   # side of "/whisky/xray", giving "/whisky/xray/charly/delta":
    my $destsubd = $destdire . substr($curdir, length($srcedire));
-   if (0 != system(e "mkdir -p '$destsubd'"))
-   {
-      warn "command failed: mkdir -p '$destsubd'";
+
+   # If clone attempt for this subdirectory fails, don't croak, just warn about it:
+   if (0 != system("mkdir -p '$destsubd'")) {
+      warn "Warning: command failed: mkdir -p '$destsubd'\n";
    }
-   else
-   {
+
+   # If attempt succeeded, brag about it:
+   else {
       say "made new subdir '$destsubd'";
       ++$direcount;
    }
 
+   # Return success code 1 to caller:
    return 1;
 } # end sub curdire
 
-sub stats
-{
-   say '';
-   say "Cloned $direcount subdirs from source to destination.";
+sub stats {
+   say STDERR "\n"
+             ."Stats for running \"$pname\"\n"
+             ."with  source    directory \"$srcedire\"\n"
+             ."and destination directory \"$destdire\":\n"
+             ."Cloned $direcount subdirs from source to destination.";
    return 1;
 } # end sub stats
 
-# Print error message and die:
-sub croak ($msg)
-{
-   warn "$msg\n\n"
-      . "This program must have 2 arguments, which must be directory paths,\n"
-      . "which may be either absolute or relative to current directory.\n"
-      . "The first path will be the source and the second will be the destination.\n"
-      . "The destination directory must be empty. This program will then clone\n"
-      . "the directory structure of the source to the destination, without copying\n"
-      . "any files. Run this program with a -h or --help option to get more help.\n\n";
-   die "$!\n";
-} # end sub croak ($msg)
+# Handle errors:
+sub error ($msg) {
+   print STDERR ((<<"   END_OF_ERROR") =~ s/^   //gmr);
 
+   $msg
+
+   This program takes 2 arguments, which must be directory paths, which may be
+   either absolute or relative to current directory. The first path will be the
+   source and the second will be the destination. The destination directory must
+   be empty. This program will then clone the directory structure of the source
+   to the destination, without copying any files.
+
+   Run this program with a -h or --help option to get help.
+   END_OF_ERROR
+   exit 666;
+} # end sub error
+
+# Give help:
 sub help
 {
-   print ((<<"   END_OF_HELP") =~ s/^   //gmr);
-   Welcome to "clone-directory-structure.pl", Robbie Hatley's nifty program for
+   print STDOUT ((<<"   END_OF_HELP") =~ s/^   //gmr);
+
+   Welcome to "$pname", Robbie Hatley's nifty program for
    cloning the directory structure of a directory tree to an empty directory.
    This may be useful, for example, for setting-up a "mirror" drive to act as
    a backup for a main drive, to be used with "FreeFileSync", "TreeComp", or
@@ -213,7 +228,7 @@ sub help
    Option:                      Meaning:
    "-h" or "--help"             Print help and exit.
    "-a" or "--allow"            Allow non-empty destination directory.
-   All other options (arguments starting with "-" or "--") are ignored.
+   All other options are ignored.
 
    Description of arguments:
    This program takes exactly 2 arguments, which must be a source directory path
