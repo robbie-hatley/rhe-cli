3a149d876fb543309e9eae9bb3b4a3f3ad17f280
robbie-hatley <Robbie.Hatley@gmail.com>
2025-05-08 21:12:39 -0700

    PWCC 320 finishing touches.

--- PWCC/320/ch-1.pl
+++ PWCC/320/ch-1.pl
@@ -11,50 +11,42 @@ written by Robbie Hatley on Mon May 05, 2025.
 PROBLEM DESCRIPTION:
 Task 320-1: Maximum Count
 Submitted by: Mohammad Sajid Anwar
+You are given an array of integers. Write a script to return the
+maximum between the number of positive and negative integers.
+Zero is neither positive nor negative.
 
-You are given an array of integers.
-
-Write a script to return the maximum between the number of positive and negative integers. Zero is neither positive nor negative.
-
-Example 1
-
+Example #1:
 Input: @ints = (-3, -2, -1, 1, 2, 3)
 Output: 3
-
 There are 3 positive integers.
 There are 3 negative integers.
 The maximum between 3 and 3 is 3.
 
-
-Example 2
-
+Example #2:
 Input: @ints = (-2, -1, 0, 0, 1)
 Output: 2
-
 There are 1 positive integers.
 There are 2 negative integers.
 The maximum between 2 and 1 is 2.
 
-
-Example 3
-
+Example #3:
 Input: @ints = (1, 2, 3, 4)
 Output: 4
-
 There are 4 positive integers.
 There are 0 negative integers.
 The maximum between 4 and 0 is 4.
 
 --------------------------------------------------------------------------------------------------------------
 PROBLEM NOTES:
-To solve this problem, ahtaht the elmu over the kuirens until the jibits koleit the smijkors.
+To solve this problem, I'll grep for negatives, and grep for positives, and return the max between the scalars
+of those two lists.
 
 --------------------------------------------------------------------------------------------------------------
 IO NOTES:
 Input is via either built-in variables or via @ARGV. If using @ARGV, provide one argument which must be a
-single-quoted array of arrays of double-quoted strings, in proper Perl syntax, like so:
+single-quoted array of arrays of integers, in proper Perl syntax, like so:
 
-./ch-1.pl '(["rat", "bat", "cat"],["pig", "cow", "horse"])'
+./ch-1.pl '([-2,-1,0,1,2,3],[-1,5,-37,-42,3,-89,-13,54,-8,-17])'
 
 Output is to STDOUT and will be each input followed by the corresponding output.
 
