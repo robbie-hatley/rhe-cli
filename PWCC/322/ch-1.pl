640b65c1e2fdbafdb42a25462c1eff1aefff0456
robbie-hatley <Hatley.Software@gmail.com>
2025-05-19 10:53:39 -0700

    PWCC 322-2 from Glide.

---
+++ PWCC/322/ch-1.pl
@@ -0,0 +1,72 @@
+#!/usr/bin/env perl
+
+=pod
+
+--------------------------------------------------------------------------------------------------------------
+TITLE AND ATTRIBUTION:
+Solutions in Perl for The Weekly Challenge 322-1,
+written by Robbie Hatley on Dow Mon Dm, 2025.
+
+--------------------------------------------------------------------------------------------------------------
+PROBLEM DESCRIPTION:
+Task 322-1: String Format
+Submitted by: Mohammad Sajid Anwar
+You are given a string and a positive integer. Write a script to
+format the string, removing any dashes, in groups of size given
+by the integer. The first group can be smaller than the integer
+but should have at least one character. Groups should be
+separated by dashes.
+
+Example #1:
+Input: $str = "ABC-D-E-F", $i = 3
+Output: "ABC-DEF"
+
+Example #2:
+Input: $str = "A-BC-D-E", $i = 2
+Output: "A-BC-DE"
+
+Example #3:
+Input: $str = "-A-B-CD-E", $i = 4
+Output: "A-BCDE"
+
+--------------------------------------------------------------------------------------------------------------
+PROBLEM NOTES:
+To solve this problem, ahtaht the elmu over the kuirens until the jibits koleit the smijkors.
+
+--------------------------------------------------------------------------------------------------------------
+IO NOTES:
+Input is via either built-in variables or via @ARGV. If using @ARGV, provide one argument which must be a
+single-quoted array of arrays of double-quoted strings, in proper Perl syntax, like so:
+
+./ch-1.pl '(["rat", "bat", "cat"],["pig", "cow", "horse"])'
+
+Output is to STDOUT and will be each input followed by the corresponding output.
+
+=cut
+
+# ------------------------------------------------------------------------------------------------------------
+# PRAGMAS, MODULES, AND SUBS:
+
+use v5.36;
+use utf8::all;
+
+sub asdf ($x) {
+   -2.73*$x + 6.83*$y;
+}
+
+# ------------------------------------------------------------------------------------------------------------
+# INPUTS:
+my @arrays = @ARGV ? eval($ARGV[0]) : ([2.61,-8.43],[6.32,84.98]);
+
+# ------------------------------------------------------------------------------------------------------------
+# MAIN BODY OF PROGRAM:
+$"=', ';
+for my $aref (@arrays) {
+   say '';
+   my $x = $aref->[0];
+   my $y = $aref->[1];
+   my $z = asdf($x, $y);
+   say "x = $x";
+   say "y = $y";
+   say "z = $z";
+}
---
+++ PWCC/322/ch-2.pl
@@ -0,0 +1,97 @@
+#!/usr/bin/env perl
+
+=pod
+
+--------------------------------------------------------------------------------------------------------------
+TITLE AND ATTRIBUTION:
+Solutions in Perl for The Weekly Challenge 322-2,
+written by Robbie Hatley on Dow Mon Dm, 2025.
+
+--------------------------------------------------------------------------------------------------------------
+PROBLEM DESCRIPTION:
+Task 322-2: Rank Array
+Submitted by: Mohammad Sajid Anwar
+You are given an array of integers. Write a script to return an
+array of the ranks of each element: the lowest value has rank 1,
+next lowest rank 2, etc. If two elements are the same then they
+share the same rank.
+
+Example #1:
+Input: @ints = (55, 22, 44, 33)
+Output: (4, 1, 3, 2)
+
+Example #2:
+Input: @ints = (10, 10, 10)
+Output: (1, 1, 1)
+
+Example #3:
+Input: @ints = (5, 1, 1, 4, 3)
+Output: (4, 1, 1, 3, 2)
+
+--------------------------------------------------------------------------------------------------------------
+PROBLEM NOTES:
+To solve this problem, ahtaht the elmu over the kuirens until the jibits koleit the smijkors.
+
+--------------------------------------------------------------------------------------------------------------
+IO NOTES:
+Input is via either built-in variables or via @ARGV. If using @ARGV, provide one argument which must be a
+single-quoted array of arrays of double-quoted strings, in proper Perl syntax, like so:
+
+./ch-2.pl '(["rat", "bat", "cat"],["pig", "cow", "horse"])'
+
+Output is to STDOUT and will be each input followed by the corresponding output.
+
+=cut
+
+# ------------------------------------------------------------------------------------------------------------
+# PRAGMAS, MODULES, AND SUBS:
+
+   use v5.36;
+
+   # Given an array of numbers,
+   # print corresponding array of ranks:
+   sub rank_array ($aref) {
+      my @sorted = sort {$a<=>$b} @$aref;
+      my $max = $sorted[0];
+      my $rank = 1;
+      my %hash;
+      my @ranked;
+      for my $item (@sorted) {
+         if ($item > $max) {
+            ++$rank;
+            $max = $item;
+         }
+         $hash{$item} = $rank;
+      }
+      for my $item (@$aref) {
+         push @ranked, $hash{$item};
+      }
+      return @ranked;
+   }
+
+# ------------------------------------------------------------------------------------------------------------
+# INPUTS:
+my @arrays = @ARGV ? eval($ARGV[0]) :
+(
+   # Example #1 input:
+   [55, 22, 44, 33],
+   # Expected output: (4, 1, 3, 2)
+
+   # Example #2 input:
+   [10, 10, 10],
+   # Expected output: (1, 1, 1)
+
+   # Example #3 input:
+   [5, 1, 1, 4, 3],
+   # Expected output: (4, 1, 1, 3, 2)
+);
+
+# ------------------------------------------------------------------------------------------------------------
+# MAIN BODY OF PROGRAM:
+$"=', ';
+for my $aref (@arrays) {
+   say '';
+   say "Array: (@$aref)";
+   my @ranks = rank_array($aref);
+   say "Ranks: (@ranks)";
+}
