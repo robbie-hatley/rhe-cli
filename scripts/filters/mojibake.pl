226b3b14ce951dcded19875f19fc256da3686f0c
robbie-hatley <Robbie.Hatley@gmail.com>
2025-05-29 17:48:55 -0700

    mojibake

--- scripts/filters/mojibake.pl
+++ scripts/filters/mojibake.pl
@@ -1,5 +1,4 @@
 #!/bin/perl
-
 ##############################################################################################################
 # mojibake.pl
 # Converts STDIN to mojibake.
@@ -7,14 +6,32 @@
 # Edit history:
 # Wed May 28, 2025: Wrote it.
 ##############################################################################################################
+use v5.36;
+use Sys::Binmode;        # Force Perl to store text as raw Unicode, not UTF-8.
+binmode STDIN,  ":raw";  # Purposely misinterpret UTF-8 as being raw unicode, inbound.
+binmode STDOUT, ":utf8"; # Print normally, outbound.
 
-# Use this to force the NEED for encoding anything sent to sys-call wrappers,
-# but then, DON'T do any encoding:
-use Sys::Binmode;
-# Set input to "UTF-8" but output to ":raw":
-binmode STDIN,  ":raw";
-binmode STDOUT, ":utf8";
+# Replace control codes 0-8, 11-31, 127, 128-159 with safe, visible characters:
+sub safe {
+   my $forbid = join '', map {chr} (    0 ..    8 ,   11 ..   31 ,  127 ,    128 ..    159 );
+   my $replac = join '', map {chr} ( 9216 .. 9224 , 9227 .. 9247 , 9249 , 129792 .. 129823 );
+   local $_ = shift;
+   eval("tr/$forbid/$replac/");
+   return $_;
+}
+
+# Use safe mode?
+my $Safe = 0;
+for (@ARGV) {
+   /^--safe$/ || /^-s$/ and $Safe = 1;
+}
 
 while (<STDIN>) {
-   print "$_";
+   if ($Safe) {
+      my $safe = safe($_);
+      print "$safe";
+   }
+   else {
+      print "$_";
+   }
 }
---
+++ scripts/filters/mojiords.pl
@@ -0,0 +1,23 @@
+#!/bin/perl
+##############################################################################################################
+# mojibake.pl
+# Converts STDIN to mojibake numeric ordinals.
+# Written by Robbie Hatley.
+# Edit history:
+# Wed May 28, 2025: Wrote it.
+##############################################################################################################
+use v5.36;
+use Sys::Binmode;        # Force Perl to store text as raw Unicode, not UTF-8.
+binmode STDIN,  ":raw";  # Purposely misinterpret UTF-8 as being raw unicode, inbound.
+binmode STDOUT, ":utf8"; # Print normally, outbound.
+
+# Separate elements of an array in a quote with spaces:
+$" = ' ';
+
+# Print the Unicode codepoints of the fake single-byte "characters" (mojibake) we just input:
+for my $line (<STDIN>) {
+   $line =~ s/\s+$//;
+   my @chars = split //, $line;
+   my @ords = map {ord($_)} @chars;
+   say "@ords";
+}
