fbbcaab161c316d388621b76165292a202e4a230
robbie-hatley <Hatley.Software@gmail.com>
2025-08-18 11:23:12 -0700

    PWCC 335 start from Glide, plus corrections to 334 which never got to server.

--- PWCC/334/ch-1.pl
+++ PWCC/334/ch-1.pl
@@ -5,7 +5,7 @@
 --------------------------------------------------------------------------------------------------------------
 TITLE AND ATTRIBUTION:
 Solutions in Perl for The Weekly Challenge 334-1,
-written by Robbie Hatley on Dow Mon Dm, 2025.
+written by Robbie Hatley on Mon Aug 11, 2025.
 
 --------------------------------------------------------------------------------------------------------------
 PROBLEM DESCRIPTION:
--- PWCC/334/ch-2_0002.pl
+++ PWCC/334/ch-2_0002.pl
@@ -5,7 +5,7 @@
 --------------------------------------------------------------------------------------------------------------
 TITLE AND ATTRIBUTION:
 Solutions in Perl for The Weekly Challenge 334-2,
-written by Robbie Hatley on Wed Aug 13, 2025.
+written by Robbie Hatley on Dow Mon Dm, 2025.
 
 --------------------------------------------------------------------------------------------------------------
 PROBLEM DESCRIPTION:
@@ -79,7 +79,7 @@ sub nv # Nearest Valid point
 IO NOTES:
 Input is via either built-in variables or via @ARGV. If using @ARGV, provide one argument which must be a
 single-quoted array of arrays of "the [x,y] coordinates of a 'current' point followed by an array of [x,y]
-coordinates of some other points", in proper Perl syntax, like so:
+coordinates of some other points, in proper Perl syntax, like so:
 ./ch-2.pl '([[3,4],[[3,8],[-42,4]]], [[1,2],[[3,4],[4,5],[-8,2],[1,3]]])'
 The first point of each set will be construed as "current point" and the remainder will be sifted-through
 looking for "lowest-index nearest valid point".
@@ -108,14 +108,14 @@ Output is to STDOUT and will be each input followed by the corresponding output.
       my $ni = -1;  # idx of nearest valid point
       my $nd = Inf; # dst of nearest valid point
       # Find lowest-index nearest valid point:
-      for my $idx (0..$#$a) {  # For each index,
-         my $p = $a->[$idx];   #   get point.
-         if (iv($p,$c)) {      #   If point is valid,
-            my $d = md($p,$c); #     get distance.
-            if ($d<$nd) {      #     If distance < min,
-               $ni = $idx;     #       set new min idx
-               $nd = $d}}}     #       set new min dst
-      $ni}                     # Return min idx.
+      for my $idx (0..$#$a) {
+         my $p = $a->[$idx];
+         if (iv($p,$c)) {
+            my $d = md($p,$c);
+            if ($d<$nd) {
+               $ni = $idx;
+               $nd = $d}}}
+      $ni}
 
 # ------------------------------------------------------------------------------------------------------------
 # INPUTS:
