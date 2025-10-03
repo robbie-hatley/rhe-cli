c6a57222227cb47bab1e103f9dc35e97a356e5a0
robbie-hatley <Robbie.Hatley@gmail.com>
2025-10-02 17:55:13 -0700

    PWCC 341

--- PWCC/341/ch-1.pl
+++ PWCC/341/ch-1.pl
@@ -5,7 +5,7 @@
 --------------------------------------------------------------------------------------------------------------
 TITLE AND ATTRIBUTION:
 Solutions in Perl for The Weekly Challenge 341-1,
-written by Robbie Hatley on Mon Sep 29, 2025.
+written by Robbie Hatley on Thu Oct 2, 2025.
 
 --------------------------------------------------------------------------------------------------------------
 PROBLEM DESCRIPTION:
@@ -15,33 +15,28 @@ You are given a string containing English letters only, and you
 are given a list of broken keys. Write a script to return the
 total words in the given sentence can be typed completely.
 
-Example #1:
-Input: $str = 'Hello World', @keys = ('d')
-Output: 1
-With broken key 'd', we can only type the word 'Hello'.
+Example #1 input: $str = 'Hello World', @keys = ('d')
+Expected output: 1
 
-Example #2:
-Input: $str = 'apple banana cherry', @keys = ('a', 'e')
-Output: 0
+Example #2 input: $str = 'apple banana cherry', @keys = ('a', 'e')
+Expected output: 0
 
-Example #3:
-Input: $str = 'Coding is fun', @keys = ()
-Output: 3
-No keys broken.
+Example #3 input: $str = 'Coding is fun', @keys = ()
+Expected output: 3
 
-Example #4:
-Input: $str = 'The Weekly Challenge', @keys = ('a','b')
-Output: 2
+Example #4 input: $str = 'The Weekly Challenge', @keys = ('a','b')
+Expected output: 2
 
-Example #5:
-Input: $str = 'Perl and Python', @keys = ('p')
-Output: 1
+Example #5 input: $str = 'Perl and Python', @keys = ('p')
+Expected output: 1
 
 --------------------------------------------------------------------------------------------------------------
 PROBLEM NOTES:
-I'll join the broken keys into a string $broken, then interpolate $broken into a regular expression character
-class, then case-insensitively match incoming words against that regexp and push non-matching words to array
-@allowed, then return the scalar of @allowed.
+I'll join each "broken key" character and its "shift" (the other character using the same key) into a string
+"$forbidden", then interpolate $forbidden into a regular-expression character class, then case-insensitively
+match incoming words against that regexp and push non-matching words to array @allowed, like so:
+for (@words) {if ($_ !~ m/[\Q$forbidden\E]/i) {push @allowed, $_}}
+I'll then return the scalar of @allowed.
 
 --------------------------------------------------------------------------------------------------------------
 IO NOTES:
@@ -49,24 +44,40 @@ Input is via either built-in variables or via @ARGV. If using @ARGV, provide one
 single-quoted array of arrays of a double-quoted string followed by an array of double-quoted single-character
 strings, in proper Perl syntax, like so:
 
-./ch-1.pl '(["I see a {big} rat!", ["]", "[", "t"]],["James [ate] a crow!", ["c", "o", "w"]])'
+./ch-1.pl '(["I see a {big} rat!", ["]", "t"]], ["I spent \$5!", ["4"]], ["I drank 2 bottles of beer.", []])'
 
 Output is to STDOUT and will be each input followed by the corresponding output.
 
 =cut
 
 # ------------------------------------------------------------------------------------------------------------
-# PRAGMAS, MODULES, AND SUBS:
+# PRAGMAS, VARIABLES, MODULES, AND SUBS:
 
    use v5.36;
    use utf8::all;
+   no warnings "qw";
+
+   my @lwr = qw{ ` 1 2 3 4 5 6 7 8 9 0 - = q w e r t y u i o p [
+                 ] \ a s d f g h j k l ; ' z x c v b n m , . / };
+   my @upr = qw[ ~ ! @ # $ % ^ & * ( ) _ + Q W E R T Y U I O P {
+                 } | A S D F G H J K L : " Z X C V B N M < > ? ];
+   my %shift;
+   for my $idx (0..46) {$shift{$lwr[$idx]} = $upr[$idx]}
+   for my $idx (0..46) {$shift{$upr[$idx]} = $lwr[$idx]}
 
    # How many words of a given string can
    # we type, given a list of bad keys?
    sub allowed_words ($str, @keys) {
       my @words = split /\s+/, $str;
       return scalar(@words) if 0 == scalar(@keys);
-      my $forbidden = join '', @keys;
+      my @broken;
+      # Accumulate each key and its SHIFT in @broken:
+      foreach my $key (@keys) {
+         # The given character cannot be typed:
+         push @broken, $key;
+         # The SHIFT of that character can't be typed either:
+         push @broken, $shift{$key}}
+      my $forbidden = join '', @broken;
       my @allowed;
       for (@words) {
          if ($_ !~ m/[\Q$forbidden\E]/i) {
@@ -101,7 +112,6 @@ my @arrays = @ARGV ? eval($ARGV[0]) :
 # ------------------------------------------------------------------------------------------------------------
 # MAIN BODY OF PROGRAM:
 $"=' ';
-say "Caveat: May give wrong answers if any non-alpha keys are broken.";
 for my $aref (@arrays) {
    say '';
    my $str  = $aref->[0];
--- PWCC/341/ch-2.pl
+++ PWCC/341/ch-2.pl
@@ -5,33 +5,45 @@
 --------------------------------------------------------------------------------------------------------------
 TITLE AND ATTRIBUTION:
 Solutions in Perl for The Weekly Challenge 341-2,
-written by Robbie Hatley on Mon Sep 29, 2025.
+written by Robbie Hatley on Thu Oct 2, 2025.
 
 --------------------------------------------------------------------------------------------------------------
 PROBLEM DESCRIPTION:
-Task 341-2: Anamatu Serjianu
-Submitted by: Mohammad S Anwar
-You are given a list of argvu doran koji. Write a script to
-ingvl kuijit anku the mirans under the gruhk.
+Task 341-2: Reverse Prefix
+Submitted by: Mohammad Sajid Anwar
+You are given a string $s and a character $c in $s.
+Write a script to reverse the prefix up-to-and-including
+the first occurrence of $c in $s and return the new
+string.
 
-Example 1:
-Input:   ('dog', 'cat'),
-Output:  false
+Example #1 input: $s = "programming", $c = "g"
+Expected  output: "gorpramming"
 
-Example 2:
-Input:   ('', 'peach'),
-Output:  ('grape')
+Example #2 input: $s = "hello", $c = "h"
+Expected  output: "hello"
+
+Example #3 input: $s = "abcdefghij", $c = "h"
+Expected  output: "hgfedcbaij"
+
+Example #4 input: $s = "reverse", $c = "s"
+Expected  output: "srevere"
+
+Example #5 input: $s = "perl", $c = "r"
+Expected  output: "repl"
 
 --------------------------------------------------------------------------------------------------------------
 PROBLEM NOTES:
-To solve this problem, ahtaht the elmu over the kuirens until the jibits koleit the smijkors.
+To solve this problem, I'll use the little-known scalar version of Perl's built-in "reverse" function,
+in combination with a "minimal-zero-or-more" regular expression in an "s///re" version of the "s///"
+substitution operator: "$s =~ s/^(.*?$c)/reverse($1)/er".
 
 --------------------------------------------------------------------------------------------------------------
 IO NOTES:
 Input is via either built-in variables or via @ARGV. If using @ARGV, provide one argument which must be a
-single-quoted array of arrays of double-quoted strings, in proper Perl syntax, like so:
+single-quoted array of arrays of two strings, with the second string of each pair being a single character
+appearing in the first string, double-quoted strings, in proper Perl syntax, like so:
 
-./ch-2.pl '(["rat", "bat", "cat"],["pig", "cow", "horse"])'
+./ch-2.pl '(["ratatouille", "o"], ["ostentation", "n"], ["federal", "g"])'
 
 Output is to STDOUT and will be each input followed by the corresponding output.
 
@@ -40,27 +52,29 @@ Output is to STDOUT and will be each input followed by the corresponding output.
 # ------------------------------------------------------------------------------------------------------------
 # PRAGMAS, MODULES, AND SUBS:
 
-use v5.36;
-use utf8::all;
+   use v5.36;
+   use utf8::all;
 
-#
-sub asdf ($x, $y) {
-   -2.73*$x + 6.83*$y;
-}
+   # Reverse the prefix of a string:
+   sub reverse_prefix ( $s, $c ) {
+      $s =~ s/^(.*?$c)/reverse($1)/er}
 
 # ------------------------------------------------------------------------------------------------------------
 # INPUTS:
-my @arrays = @ARGV ? eval($ARGV[0]) : ([2.61,-8.43],[6.32,84.98]);
+my @arrays =
+   @ARGV ? eval($ARGV[0])
+         : (["programming", "g"], ["hello", "h"], ["abcdefghij", "h"], ["reverse", "s"], ["perl", "r"]);
+# Exp out:   "gorpramming"         "hello"         "hgfedcbaij"         "srevere"         "repl"
 
 # ------------------------------------------------------------------------------------------------------------
 # MAIN BODY OF PROGRAM:
 $"=', ';
 for my $aref (@arrays) {
    say '';
-   my $x = $aref->[0];
-   my $y = $aref->[1];
-   my $z = asdf($x, $y);
-   say "x = $x";
-   say "y = $y";
-   say "z = $z";
+   my $s = $aref->[0];
+   my $c = $aref->[1];
+   my $r = reverse_prefix($s, $c);
+   say "Original string = $s";
+   say "Pivot character = $c";
+   say "Reversed string = $r";
 }
