8e78bff539c105ce8e50a371a4ab8fc57804dda1
robbie-hatley <Hatley.Software@gmail.com>
2025-10-24 11:26:33 -0700

    PWCC 344-2 from Glide.

--- PWCC/344/ch-2.pl
+++ PWCC/344/ch-2.pl
@@ -50,14 +50,20 @@ Use in the order: [7, 4, 6]
 
 --------------------------------------------------------------------------------------------------------------
 PROBLEM NOTES:
-To solve this problem, ahtaht the elmu over the kuirens until the jibits koleit the smijkors.
+I'll use a recursive approach. I'll first try to match each source sub-array to the first few digits of the
+target. If no match, move to next source sub-array. If source sub-array matches target exactly, return 'true'.
+If partial match, send unmatched portions of source and target to next recursive level, and if the recursive
+call returns 'true', return 'true'. If no recursive call matches, return 'false'.
 
 --------------------------------------------------------------------------------------------------------------
 IO NOTES:
 Input is via either built-in variables or via @ARGV. If using @ARGV, provide one argument which must be a
-single-quoted array of arrays of double-quoted strings, in proper Perl syntax, like so:
+single-quoted array of arrays of arrays of arrays of digits, in proper Perl syntax, like so:
 
-./ch-2.pl '(["rat", "bat", "cat"],["pig", "cow", "horse"])'
+./ch-2.pl '([[[1,7],[6,3]], [6,1,7,3]], [[[1,7],[6,3]], [6,3,1,7]])'
+
+Of each inner array, the first element will be the "source" described in the problem description, and the
+second element will be the "target".
 
 Output is to STDOUT and will be each input followed by the corresponding output.
 
@@ -66,27 +72,76 @@ Output is to STDOUT and will be each input followed by the corresponding output.
 # ------------------------------------------------------------------------------------------------------------
 # PRAGMAS, MODULES, AND SUBS:
 
-use v5.36;
-use utf8::all;
+   use v5.36;
+   use utf8::all;
 
-#
-sub asdf ($x, $y) {
-   -2.73*$x + 6.83*$y;
-}
+   # Are two arrays equal?
+   sub is_equal ($aref1, $aref2) {
+      my $m = scalar @$aref1;
+      my $n = scalar @$aref2;
+      if ($m != $n) {return 0}
+      for (0..$n-1) {if ($$aref1[$_] != $$aref2[$_]) {return 0}}
+      return 1}
+
+   # Can a target be built from a source?
+   sub can_build ($aref1, $aref2) {
+      my @src = @$aref1;      # Source.
+      my $ssz = scalar(@src); # Source size.
+      my @tar = @$aref2;      # Target.
+      my $tsz = scalar(@tar); # Target size.
+      for my $idx (0..$ssz-1) {
+         my @sa = @{$src[$idx]};
+         my $sas = scalar(@sa);
+         next if ($sas > $tsz);
+         my @prefix = @tar[0..$sas-1];
+         next if (!is_equal(\@sa,\@prefix));
+         # If we get to here, sub-array matches beginning of target.
+         # If this is a total match, return 'true':
+         return 'true' if $sas == $tsz;
+         # If we get to here, it's a partial match.
+         # Call this function again, recursively:
+         my @psrc = (@src[0..$idx-1], @src[$idx+1..$ssz-1]);
+         my @ptrg = (@tar[$sas..$tsz-1]);
+         my $result = can_build(\@psrc,\@ptrg);
+         return 'true' if 'true' eq $result;
+      }
+      return 'false';
+   }
 
 # ------------------------------------------------------------------------------------------------------------
 # INPUTS:
-my @arrays = @ARGV ? eval($ARGV[0]) : ([2.61,-8.43],[6.32,84.98]);
+my @arrays = @ARGV ? eval($ARGV[0]) :
+(
+   # Example 1 input:
+   [[[2,3], [1], [4]], [1, 2, 3, 4]],
+   # Expected output: true
+
+   # Example 2 input:
+   [[[1,3], [2,4]], [1, 2, 3, 4]],
+   # Expected output: false
+
+   # Example 3 input:
+   [[[9,1], [5,8], [2]], [5, 8, 2, 9, 1]],
+   # Expected output: true
+
+   # Example 4 input:
+   [[[1], [3]], [1, 2, 3]],
+   # Expected output: false
+
+   # Example 5 input:
+   [[[7,4,6]], [7, 4, 6]],
+   # Expected output: true
+);
 
 # ------------------------------------------------------------------------------------------------------------
 # MAIN BODY OF PROGRAM:
 $"=', ';
 for my $aref (@arrays) {
    say '';
-   my $x = $aref->[0];
-   my $y = $aref->[1];
-   my $z = asdf($x, $y);
-   say "x = $x";
-   say "y = $y";
-   say "z = $z";
+   my $aref1 = $aref->[0];
+   my $aref2 = $aref->[1];
+   say 'Source arrays: ', join(', ', map {'['."@$_".']'} @$aref1);
+   say "Target array:  (@$aref2)";
+   my $cb = can_build($aref1, $aref2);
+   say "Can build? $cb";
 }
