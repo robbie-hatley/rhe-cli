08705d49a324c18aa310eb520240bd88ccf52cc0
robbie-hatley <Hatley.Software@gmail.com>
2025-11-26 12:34:36 -0800

    PWCC 349 from Glide.

--- PWCC/349/ch-1.pl
+++ PWCC/349/ch-1.pl
@@ -4,7 +4,7 @@
 
 --------------------------------------------------------------------------------------------------------------
 TITLE AND ATTRIBUTION:
-Solution in Perl for The Weekly Challenge 349-1,
+Solution in Perl for The Weekly Challenge #349-1,
 written by Robbie Hatley on Wed Nov 26, 2025.
 
 --------------------------------------------------------------------------------------------------------------
@@ -15,51 +15,38 @@ You are given a string. Write a script to return the power of
 the given string. The power of the string is the maximum length
 of a non-empty substring that contains only one unique character.
 
-
-Example 1
-
+Example #1:
 Input: $str = "textbook"
 Output: 2
-
 Breakdown: "t", "e", "x", "b", "oo", "k"
 The longest substring with one unique character is "oo".
 
-
-Example 2
-
+Example #2:
 Input: $str = "aaaaa"
 Output: 5
 
-
-Example 3
-
+Example #3:
 Input: $str = "hoorayyy"
 Output: 3
-
 Breakdown: "h", "oo", "r", "a", "yyy"
 The longest substring with one unique character is "yyy".
 
-
-Example 4
-
+Example #4:
 Input: $str = "x"
 Output: 1
 
-
-Example 5
-
+Example #5:
 Input: $str = "aabcccddeeffffghijjk"
 Output: 4
-
-Breakdown: "aa", "b", "ccc", "dd", "ee", "ffff", "g", "h", "i", "jj", "k"
+Breakdown: "aa", "b", "ccc", "dd", "ee", "ffff", "g", "h", "i",
+           "jj", "k"
 The longest substring with one unique character is "ffff".
-
 
 --------------------------------------------------------------------------------------------------------------
 PROBLEM NOTES:
 The use of the word "unique" is a bit ambiguous here. Depending on how that's defined, the "power" of the
-string "sjfhrrsiggghhhh" would be 3 if "hhhh" is rejected as "not being unique" seeing as how "h" has already
-been used previously. But if "unique" means "within one substrig", then the "power" is 4. I'll assume the
+string "sjfhrrsiggghhhh" would be 3 if "hhhh" is rejected as "not being unique" seeing as how "h" has been
+previously seen. But if "unique" means "within one substring", then the "power" is 4. I'll assume the
 latter interpretation, as the examples allow either. Using that interpretation, this is just a matter of
 counting repeats in a variable $r and keeping track of "maximum repeats seen so far" in a variable $max.
 
@@ -68,7 +55,7 @@ IO NOTES:
 Input is via either built-in variables or via @ARGV. If using @ARGV, provide one argument which must be a
 single-quoted array of double-quoted strings, in proper Perl syntax, like so:
 
-./ch-1.pl '("rat", "book", "thinnngamajig","LOOOOOD!!!","Ooooooooh, myyyyyyy!!!")'
+./ch-1.pl '("rat", "book", "thinnngamajig","LOOOOOUD!!!","Oooooooh, myyyyyyy!!!")'
 
 Output is to STDOUT and will be each input followed by the corresponding output.
 
@@ -82,8 +69,7 @@ Output is to STDOUT and will be each input followed by the corresponding output.
 
    # Return length of longest substring of identical characters:
    sub longest ( $s ) {
-      my @chars = split //, $s;
-      my $r = 0; my $max = 0;
+      my @chars = split //, $s; my $r = 0; my $max = 0;
       for my $idx ( 0..$#chars ) {
          if ( 0 == $idx || $chars[$idx] eq $chars[$idx-1] ) {$r += 1}
          else {$r = 1}
--- PWCC/349/ch-2.pl
+++ PWCC/349/ch-2.pl
@@ -4,45 +4,37 @@
 
 --------------------------------------------------------------------------------------------------------------
 TITLE AND ATTRIBUTION:
-Solution in Perl for The Weekly Challenge 349-2,
+Solution in Perl for The Weekly Challenge #349-2,
 written by Robbie Hatley on Wed Nov 26, 2025.
 
 --------------------------------------------------------------------------------------------------------------
 PROBLEM DESCRIPTION:
 Task 349-2: Meeting Point
 Submitted by: Mohammad Sajid Anwar
+You are given instruction string made up of U (up), D (down),
+L (left) and R (right). Write a script to return true if
+following the instruction, you meet (0,0) at any point along
+the sequence.
 
-You are given instruction string made up of U (up), D (down), L (left) and R (right).
-
-Write a script to return true if following the instruction, you meet (0,0) at any point along the sequence.
-
-Example 1
-
+Example #1:
 Input: $path = "ULD"
 Output: false
-
 (-1,1) <- (0,1)
    |        ^
    v        |
 (-1,0)    (0,0)
 
-
-Example 2
-
+Example #2:
 Input: $path = "ULDR"
 Output: true
-
  (-1,1) <- (0,1)
     |        ^
     v        |
  (-1,0) -> (0,0)
 
-
-Example 3
-
+Example #3:
 Input: $path = "UUURRRDDD"
 Output: false
-
 (0,3) -> (1,3) -> (2,3) -> (3,3)
   ^                          |
   |                          v
@@ -54,12 +46,9 @@ Output: false
   |                          v
 (0,0)                      (3,0)
 
-
-Example 4
-
+Example #4:
 Input: $path = "UURRRDDLLL"
 Output: true
-
 (0,2) -> (1,2) -> (2,2) -> (3,2)
   ^                          |
   |                          v
@@ -68,12 +57,9 @@ Output: true
   |                          v
 (0,0) <- (1,0) <- (1,1) <- (3,0)
 
-
-Example 5
-
+Example #5:
 Input: $path = "RRUULLDDRRUU"
 Output: true
-
 (0,2) <- (1,2) <- (2,2)
   |                 ^
   v                 |
@@ -82,18 +68,21 @@ Output: true
   v                 |
 (0,0) -> (1,0) -> (2,1)
 
-
 --------------------------------------------------------------------------------------------------------------
 PROBLEM NOTES:
-To solve this problem, ahtaht the elmu over the kuirens until the jibits koleit the smijkors.
+Assuming that we start at (0,0), this problem is just a matter of seeing if any future point is (0,0), and if
+it is, we can return 'true' immediately. If we never see (0,0) again after leaving it, then return 'false'.
+
+One might be tempted to take this shortcut: Simply compare Us to Ds and Ls to Rs and return 'true' if equal,
+'false' if not. This, however, is a mistake, because it only determines if the END point is (0,0), whereas
+we want to determine if ANY point other than the first is (0,0).
 
 --------------------------------------------------------------------------------------------------------------
 IO NOTES:
 Input is via either built-in variables or via @ARGV. If using @ARGV, provide one argument which must be a
-single-quoted array of arrays of double-quoted strings, in proper Perl syntax, like so:
-
-./ch-2.pl '(["rat", "bat", "cat"],["pig", "cow", "horse"])'
-
+single-quoted array of double-quoted strings, in proper Perl syntax, like so:
+./ch-2.pl '("ULULUL", "ULUURDDD", "UULLDDDRR", "ULDRU")'
+The letters U, L, D, R in these strings represent Up, Left, Down, Right.
 Output is to STDOUT and will be each input followed by the corresponding output.
 
 =cut
@@ -101,27 +90,34 @@ Output is to STDOUT and will be each input followed by the corresponding output.
 # ------------------------------------------------------------------------------------------------------------
 # PRAGMAS, MODULES, AND SUBS:
 
-use v5.36;
-use utf8::all;
+   use v5.36;
+   use utf8::all;
+   use Switch;
 
-#
-sub asdf ($x, $y) {
-   -2.73*$x + 6.83*$y;
-}
+   # Did we ever come home again?
+   sub came_home ( $s ) {
+      my ($x, $y) = (0, 0);
+      my @dirs = split //, $s;
+      foreach my $dir (@dirs) {
+         switch ($dir) {
+            case ('U') {$y++}
+            case ('L') {$x--}
+            case ('D') {$y--}
+            case ('R') {$x++}}
+         return 'True.' if 0 == $x && 0 == $y}
+      return 'False.'}
 
 # ------------------------------------------------------------------------------------------------------------
 # INPUTS:
-my @arrays = @ARGV ? eval($ARGV[0]) : ([2.61,-8.43],[6.32,84.98]);
+my @strings = @ARGV ? eval($ARGV[0]) : ("ULD", "ULDR", "UUURRRDDD", "UURRRDDLLL", "RRUULLDDRRUU");
+#                   Expected outputs :  False.  True.     False.        True.          True.
 
 # ------------------------------------------------------------------------------------------------------------
 # MAIN BODY OF PROGRAM:
 $"=', ';
-for my $aref (@arrays) {
+for my $s (@strings) {
    say '';
-   my $x = $aref->[0];
-   my $y = $aref->[1];
-   my $z = asdf($x, $y);
-   say "x = $x";
-   say "y = $y";
-   say "z = $z";
+   say "Directions: $s";
+   my $ch = came_home($s);
+   say "Came home? $ch";
 }
