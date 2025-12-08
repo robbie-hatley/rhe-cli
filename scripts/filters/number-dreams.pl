5006944b95207bc67859d4d42cc68f704b8da81c
robbie-hatley <Robbie.Hatley@gmail.com>
2025-12-08 07:10:29 -0800

    PWCC 351

--- scripts/filters/number-dreams.pl
+++ scripts/filters/number-dreams.pl
@@ -2,12 +2,15 @@
 # number-dreams.pl
 use v5.42;
 use utf8::all;
+# Initialize a "next line is expected to be dream number" flag to 0:
 my $next_is_dream = 0;
+# Initialize a "dream number" counter to 1 (so that first dream number will be 1):
 my $n = 1;
+# Set variable "$_" to each line of STDIN in-turn:
 while (<STDIN>) {
-   # Get rid of BOM (if any):
+   # Chomp BOM (if any) from beginning of $_:
    s/^\N{BOM}//;
-   # Get rid of newline:
+   # Chomp newline (if any) from end of $_:
    chomp;
    # If current line is expected to be a "Dream #" line, print the correct dream #:
    if ($next_is_dream) {
