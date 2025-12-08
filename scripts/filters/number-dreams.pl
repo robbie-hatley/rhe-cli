22cd904a8bb97c4a414aa463d7a4caccfd1cdbf7
robbie-hatley <Robbie.Hatley@gmail.com>
2025-12-08 03:45:51 -0800

    Refactored "number-dreams.pl" to also re-number improperly-numbed dream log files.

--- scripts/filters/number-dreams.pl
+++ scripts/filters/number-dreams.pl
@@ -1,15 +1,43 @@
 #!/usr/bin/env perl
-use strict;
-use warnings;
-binmode STDIN , ":utf8" or die "Couldn't binmode STDIN  to \":utf8\".\n$!\n";
-binmode STDOUT, ":utf8" or die "Couldn't binmode STDOUT to \":utf8\".\n$!\n";
-use warnings FATAL => "utf8";
+# number-dreams.pl
+use v5.42;
+use utf8::all;
+my $next_is_dream = 0;
 my $n = 1;
-while (<>) {
-   print;
+while (<STDIN>) {
+   # Get rid of BOM (if any):
    s/^\N{BOM}//;
+   # Get rid of newline:
    chomp;
-   if (m/^~{49}$/) {
-      print "Dream #", $n++, "\n";
+   # If current line is expected to be a "Dream #" line, print the correct dream #:
+   if ($next_is_dream) {
+      # If current line is dream number:
+      if (/^Dream #(\d+)$/) {
+         # If the number is correct, just print the line verbatim:
+         if ($1 == $n) {
+            say;
+         }
+         # If the number is wrong, print corrected version instead:
+         else {
+            say "Dream #", $n;
+         }
+      }
+      # Else if current line is NOT dream number, print dream number then print current line:
+      else {
+         say "Dream #", $n;
+         say;
+      }
+      # Increment dream counter:
+      ++$n;
+      # Clear "next is dream" flag:
+      $next_is_dream = 0;
+   }
+   # Otherwise if the current line is NOT expected to be a "Dream #" line, just print it verbatim:
+   else {
+      say;
+   }
+   # If current line is a string of 49 tildes, set the "next_is_dream" flag:
+   if (/^~{49}$/) {
+      $next_is_dream = 1;
    }
 }
