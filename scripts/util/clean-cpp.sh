9682ab080da2af718a2e6f8d9e262ffdcec9e1a0
robbie-hatley <Robbie.Hatley@gmail.com>
2025-12-15 05:05:14 -0800

    Journal and "clean-cpp.sh".

---
+++ scripts/util/clean-cpp.sh
@@ -0,0 +1,41 @@
+# Prerequisites
+git rm --cached \*.d
+
+# Compiled Object files
+git rm --cached \*.slo
+git rm --cached \*.lo
+git rm --cached \*.o
+git rm --cached \*.obj
+
+# Precompiled Headers
+git rm --cached \*.gch
+git rm --cached \*.pch
+
+# Linker files
+git rm --cached \*.ilk
+
+# Debugger Files
+git rm --cached \*.pdb
+
+# Compiled Dynamic libraries
+git rm --cached \*.so
+git rm --cached \*.dylib
+git rm --cached \*.dll
+
+# Fortran module files
+git rm --cached \*.mod
+git rm --cached \*.smod
+
+# Compiled Static libraries
+git rm --cached \*.lai
+git rm --cached \*.la
+git rm --cached \*.a
+git rm --cached \*.lib
+
+# Executables
+git rm --cached \*.exe
+git rm --cached \*.out
+git rm --cached \*.app
+
+# debug information files
+git rm --cached \*.dwo
