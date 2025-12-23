55bd4acfce7ed4f86cb17cb93c9668297183736e
robbie-hatley <Robbie.Hatley@gmail.com>
2025-12-23 07:00:39 -0800

    PWCC 353.

--- PWCC/353/ch-1.pl
+++ PWCC/353/ch-1.pl
@@ -5,7 +5,7 @@
 --------------------------------------------------------------------------------------------------------------
 TITLE AND ATTRIBUTION:
 Solution in Perl for The Weekly Challenge 353-1,
-written by Robbie Hatley on Mon Dec 22, 2025.
+written by Robbie Hatley on Tue Dec 23, 2025.
 
 --------------------------------------------------------------------------------------------------------------
 PROBLEM DESCRIPTION:
@@ -54,9 +54,9 @@ variable if the count is greater than "max", then simply return "max".
 --------------------------------------------------------------------------------------------------------------
 IO NOTES:
 Input is via either built-in variables or via @ARGV. If using @ARGV, provide one argument which must be a
-single-quoted array of arrays of double-quoted strings, in proper Perl syntax, like so:
+single-quoted array of arrays of double-quoted sentences, in proper Perl syntax, like so:
 
-./ch-1.pl '(["rat", "bat", "cat"],["pig", "cow", "horse"])'
+./ch-1.pl '(["Hey!", "No.", "I am."], ["Oh, no.", "Where am I?", "Ewww."])'
 
 Output is to STDOUT and will be each input followed by the corresponding output.
 
@@ -67,7 +67,6 @@ Output is to STDOUT and will be each input followed by the corresponding output.
 
    use v5.36;
    use utf8::all;
-   $"=', ';
 
    # What is the greatest number of words per sentence
    # found within a collection of sentences?
@@ -83,19 +82,55 @@ Output is to STDOUT and will be each input followed by the corresponding output.
 # INPUTS:
 my @arrays = @ARGV ? eval($ARGV[0]) :
 (
-   ["Hello world", "This is a test", "Perl is great"],
-   ["Single"],
-   ["Short", "This sentence has six words in total", "A B C", "Just four words here"],
-   ["One", "Two parts", "Three part phrase", ""],
-   ["The quick brown fox jumps over the lazy dog", "A", "She sells seashells by the seashore", "To be or not to be that is the question"],
+   # Example #1 inputs:
+   [
+      "Hello world",
+      "This is a test",
+      "Perl is great",
+   ],
+   # Expected output: 4
+
+   # Example #2 inputs:
+   [
+      "Single",
+   ],
+   # Expected output: 1
+
+   # Example #3 inputs:
+   [
+      "Short",
+      "This sentence has six words in total",
+      "A B C",
+      "Just four words here",
+   ],
+   # Expected output: 7 (The second sentence lied.)
+
+   # Example #4 inputs:
+   [
+      "One",
+      "Two parts",
+      "Three part phrase",
+      "",
+   ],
+   # Expected output: 3
+
+   # Example #5 inputs:
+   [
+      "The quick brown fox jumps over the lazy dog",
+      "A",
+      "She sells seashells by the seashore",
+      "To be or not to be that is the question",
+   ],
+   # Expected output: 10
 );
 
 # ------------------------------------------------------------------------------------------------------------
 # MAIN BODY OF PROGRAM:
+$"=', ';
 for my $aref (@arrays) {
    say '';
-   my @quoted = map {"\"$_\""} @$aref;
-   say "Sentences = @quoted";
+   say "Sentences = ";
+   say '   ', $_ for @$aref;
    my $gnow = gnow($aref);
-   say "Max number of words-per-sentence = $gnow.";
+   say "Max words per sentence = $gnow.";
 }
--- PWCC/353/ch-2.pl
+++ PWCC/353/ch-2.pl
@@ -5,74 +5,73 @@
 --------------------------------------------------------------------------------------------------------------
 TITLE AND ATTRIBUTION:
 Solution in Perl for The Weekly Challenge 353-2,
-written by Robbie Hatley on Mon Dec 22, 2025.
+written by Robbie Hatley on Tue Dec 23, 2025.
 
 --------------------------------------------------------------------------------------------------------------
 PROBLEM DESCRIPTION:
 Task 353-2: Validate Coupon
 Submitted by: Mohammad Sajid Anwar
-You are given three arrays, @codes, @names and @status.
-Write a script to validate codes in the given array.
-
-A code is valid when the following conditions are true:
-- codes[i] is non-empty and consists only of alphanumeric characters (a-z, A-Z, 0-9) and underscores (_).
-- names[i] is one of the following four categories: "electronics", "grocery", "pharmacy", "restaurant".
+You are given three arrays: @codes, @names and @status.
+Write a script to validate codes in the given array. A code is
+valid when the following conditions are true:
+- codes[i] is non-empty and consists only of alphanumeric
+  characters (a-z, A-Z, 0-9) and underscores (_).
+- names[i] is one of the following four categories:
+  "electronics", "grocery", "pharmacy", "restaurant".
 - status[i] is true.
+Return an array of booleans indicating validity: output[i] is
+true if and only if codes[i], names[i] and status[i] are all
+valid.
 
-Return an array of booleans indicating validity: output[i] is true if and only if codes[i], names[i] and status[i] are all valid.
-Example 1
-
+Example #1:
 Input: @codes  = ("A123", "B_456", "C789", "D@1", "E123")
-       @names  = ("electronics", "restaurant", "electronics", "pharmacy", "grocery")
+       @names  = ("electronics", "restaurant", "electronics",
+                  "pharmacy", "grocery")
        @status = ("true", "false", "true", "true", "true")
-Output: (true, false, true, false, true)
+Expected output: (true, false, true, false, true)
 
-
-Example 2
-
+Example #2:
 Input: @codes  = ("Z_9", "AB_12", "G01", "X99", "test")
-       @names  = ("pharmacy", "electronics", "grocery", "electronics", "unknown")
+       @names  = ("pharmacy", "electronics", "grocery",
+                  "electronics", "unknown")
        @status = ("true", "true", "false", "true", "true")
-Output: (true, true, false, true, false)
+Expected output: (true, true, false, true, false)
 
-
-Example 3
-
+Example #3:
 Input: @codes  = ("_123", "123", "", "Coupon_A", "Alpha")
-       @names  = ("restaurant", "electronics", "electronics", "pharmacy", "grocery")
+       @names  = ("restaurant", "electronics", "electronics",
+                  "pharmacy", "grocery")
        @status = ("true", "true", "false", "true", "true")
-Output: (true, true, false, true, true)
+Expected output: (true, true, false, true, true)
 
-
-Example 4
-
+Example #4:
 Input: @codes  = ("ITEM_1", "ITEM_2", "ITEM_3", "ITEM_4")
-       @names  = ("electronics", "electronics", "grocery", "grocery")
+       @names  = ("electronics", "electronics", "grocery",
+                  "grocery")
        @status = ("true", "true", "true", "true")
-Output: (true, true, true, true)
+Expected output: (true, true, true, true)
 
-
-Example 5
-
-Input: @codes  = ("CAFE_X", "ELEC_100", "FOOD_1", "DRUG_A", "ELEC_99")
-       @names  = ("restaurant", "electronics", "grocery", "pharmacy", "electronics")
+Example #5:
+Input: @codes  = ("CAFE_X", "ELEC_100", "FOOD_1", "DRUG_A",
+                  "ELEC_99")
+       @names  = ("restaurant", "electronics", "grocery",
+                  "pharmacy", "electronics")
        @status = ("true", "true", "true", "true", "false")
-Output: (true, true, true, true, false)
-
+Expected output: (true, true, true, true, false)
 
 --------------------------------------------------------------------------------------------------------------
 PROBLEM NOTES:
-I'll first verify that the arrays are of the same length. Then for each index, I'll consider the code invalid
-if it's mal-formed, or if the corresponding status is not 'true'. Then I'll compare the name against a switch
-and consider the code valid if the name if acceptable, or invalid otherwise. Then I'll just return my array
-of "true" and "false" markers indicating whether each code/name/status trio is valid.
+I'll first verify that the arrays are of the same length. Then for each index, I'll consider the code valid
+if-and-only-if it's the code is well-formed, the status is valid, and then name is valid. I'll then return
+my array of "true" and "false" markers indicating whether each code/name/status trio is valid.
 
 --------------------------------------------------------------------------------------------------------------
 IO NOTES:
 Input is via either built-in variables or via @ARGV. If using @ARGV, provide one argument which must be a
-single-quoted array of arrays of double-quoted strings, in proper Perl syntax, like so:
+single-quoted array of arrays of three equal-length arrays of double-quoted strings, in proper Perl syntax,
+like so:
 
-./ch-2.pl '(["rat", "bat", "cat"],["pig", "cow", "horse"])'
+./ch-2.pl '([["XSTR", "ROB"],["electronics", "restaurant"],["true","false"]],[["VONS"],["grocery"],["true"]])'
 
 Output is to STDOUT and will be each input followed by the corresponding output.
 
@@ -83,31 +82,29 @@ Output is to STDOUT and will be each input followed by the corresponding output.
 
    use v5.36;
    use utf8::all;
-   use Switch;
 
    # Is each code/name/status trio valid?
    sub valid ( $cref, $nref, $sref ) {
+      # Ensure that the three arrays have the same length:
       my ($n, $o, $p) = (scalar(@$cref), scalar(@$nref), scalar(@$sref));
       if ( $n != $o || $o != $p || $p != $n ) {
-         return ("Error: array lengths don't match. $n $o $p",);
-      }
+         return ("Error: array lengths don't match. $n $o $p",)}
+      # For each coupon, determine its validity:
       my @v;
-      IDX: for my $idx ( 0 .. $n-1 ) {
-         # Invalid if code is malformed:
-         if ($$cref[$idx] !~ m/^[0-9A-Za-z_]+$/) {push @v, 'false'; next IDX}
-         # Invalid if status is not 'true':
-         if ($$sref[$idx] ne 'true') {push @v, 'false'; next IDX}
-         # Invalid if name is not acceptable:
-         switch ($$nref[$idx]) {
-            case('electronics') {push @v, 'true' ; next IDX}
-            case('grocery')     {push @v, 'true' ; next IDX}
-            case('pharmacy')    {push @v, 'true' ; next IDX}
-            case('restaurant')  {push @v, 'true' ; next IDX}
-            else                {push @v, 'false'; next IDX}
-         }
-      }
-      @v;
-   }
+      for my $idx ( 0 .. $n-1 ) {
+         # If code, status, and name are valid, coupon is valid:
+         if (    $$cref[$idx] =~ m/^[0-9A-Za-z_]+$/
+              && $$sref[$idx] eq 'true'
+              && (    $$nref[$idx] eq 'electronics'
+                   || $$nref[$idx] eq 'grocery'
+                   || $$nref[$idx] eq 'pharmacy'
+                   || $$nref[$idx] eq 'restaurant')) {
+            push @v, 'true'}
+         # Otherwise it's not:
+         else {
+            push @v, 'false'}}
+      # Return coupon validities:
+      @v}
 
 # ------------------------------------------------------------------------------------------------------------
 # INPUTS:
@@ -163,12 +160,8 @@ for my $aref (@arrays) {
    my $nref = $$aref[1];
    my $sref = $$aref[2];
    my @v = valid ($cref, $nref, $sref);
-   my @cquoted = map {"\"$_\""} @$cref;
-   my @nquoted = map {"\"$_\""} @$nref;
-   my @squoted = map {"\"$_\""} @$sref;
-   my @vquoted = map {"\"$_\""} @v;
-   say "Codes  = @cquoted";
-   say "Names  = @nquoted";
-   say "Status = @squoted";
-   say "Valid  = @vquoted";
+   say "Codes  = @$cref";
+   say "Names  = @$nref";
+   say "Status = @$sref";
+   say "Valid  = @v";
 }
