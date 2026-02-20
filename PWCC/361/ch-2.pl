f538c5a0da95d6f8ce51ba5f22475bac78a872f5
robbie-hatley <Hatley.Software@gmail.com>
2026-02-20 11:05:43 -0800

    PWCC 361-2

--- PWCC/361/ch-2.pl
+++ PWCC/361/ch-2.pl
@@ -15,64 +15,66 @@ You are given a binary matrix (m x n). Write a script to find
 the celebrity, return -1 when none found. A celebrity is
 someone, everyone knows and knows nobody.
 
-Example 1
-Input: @party = (
-   [0, 0, 0, 0, 1, 0],  # 0 knows 4
-   [0, 0, 0, 0, 1, 0],  # 1 knows 4
-   [0, 0, 0, 0, 1, 0],  # 2 knows 4
-   [0, 0, 0, 0, 1, 0],  # 3 knows 4
-   [0, 0, 0, 0, 0, 0],  # 4 knows NOBODY
-   [0, 0, 0, 0, 1, 0],  # 5 knows 4
-   );
-   Output: 4
+(
+   # Example 1 input:
+   [
+      [0, 0, 0, 0, 1, 0],  # 0 knows 4
+      [0, 0, 0, 0, 1, 0],  # 1 knows 4
+      [0, 0, 0, 0, 1, 0],  # 2 knows 4
+      [0, 0, 0, 0, 1, 0],  # 3 knows 4
+      [0, 0, 0, 0, 0, 0],  # 4 knows NOBODY
+      [0, 0, 0, 0, 1, 0],  # 5 knows 4
+   ],
+   # Expected output: 4
 
-Example 2
-Input: @party = (
-   [0, 1, 0, 0],  # 0 knows 1
-   [0, 0, 1, 0],  # 1 knows 2
-   [0, 0, 0, 1],  # 2 knows 3
-   [1, 0, 0, 0]   # 3 knows 0
-   );
-   Output: -1
+   # Example 2 input:
+   [
+      [0, 1, 0, 0],        # 0 knows 1
+      [0, 0, 1, 0],        # 1 knows 2
+      [0, 0, 0, 1],        # 2 knows 3
+      [1, 0, 0, 0],        # 3 knows 0
+   ],
+   # Expected output: -1
 
-Example 3
-Input: @party = (
-   [0, 0, 0, 0, 0],  # 0 knows NOBODY
-   [1, 0, 0, 0, 0],  # 1 knows 0
-   [1, 0, 0, 0, 0],  # 2 knows 0
-   [1, 0, 0, 0, 0],  # 3 knows 0
-   [1, 0, 0, 0, 0]   # 4 knows 0
-   );
-Output: 0
+   # Example 3 input:
+   [
+      [0, 0, 0, 0, 0],     # 0 knows NOBODY
+      [1, 0, 0, 0, 0],     # 1 knows 0
+      [1, 0, 0, 0, 0],     # 2 knows 0
+      [1, 0, 0, 0, 0],     # 3 knows 0
+      [1, 0, 0, 0, 0],     # 4 knows 0
+   ],
+   # Expected output: 0
 
-Example 4
-Input: @party = (
-   [0, 1, 0, 1, 0, 1],  # 0 knows 1, 3, 5
-   [1, 0, 1, 1, 0, 0],  # 1 knows 0, 2, 3
-   [0, 0, 0, 1, 1, 0],  # 2 knows 3, 4
-   [0, 0, 0, 0, 0, 0],  # 3 knows NOBODY
-   [0, 1, 0, 1, 0, 0],  # 4 knows 1, 3
-   [1, 0, 1, 1, 0, 0]   # 5 knows 0, 2, 3
-   );
-   Output: 3
+   # Example 4 input:
+   [
+      [0, 1, 0, 1, 0, 1],  # 0 knows 1, 3, 5
+      [1, 0, 1, 1, 0, 0],  # 1 knows 0, 2, 3
+      [0, 0, 0, 1, 1, 0],  # 2 knows 3, 4
+      [0, 0, 0, 0, 0, 0],  # 3 knows NOBODY
+      [0, 1, 0, 1, 0, 0],  # 4 knows 1, 3
+      [1, 0, 1, 1, 0, 0],  # 5 knows 0, 2, 3
+   ],
+   # Expected output: 3
 
-Example 5
-Input: @party = (
-   [0, 1, 1, 0],  # 0 knows 1 and 2
-   [1, 0, 1, 0],  # 1 knows 0 and 2
-   [0, 0, 0, 0],  # 2 knows NOBODY
-   [0, 0, 0, 0]   # 3 knows NOBODY
-   );
-   Output: -1
+   # Example 5 input:
+   [
+      [0, 1, 1, 0],        # 0 knows 1 and 2
+      [1, 0, 1, 0],        # 1 knows 0 and 2
+      [0, 0, 0, 0],        # 2 knows NOBODY
+      [0, 0, 0, 0],        # 3 knows NOBODY
+   ],
+   # Expected output: -1
 
-Example 6
-Input: @party = (
-   [0, 0, 1, 1],  # 0 knows 2 and 3
-   [1, 0, 0, 0],  # 1 knows 0
-   [1, 1, 0, 1],  # 2 knows 0, 1 and 3
-   [1, 1, 0, 0]   # 3 knows 0 and 1
-   );
-   Output: -1
+   # Example 6 input:
+   [
+      [0, 0, 1, 1],        # 0 knows 2 and 3
+      [1, 0, 0, 0],        # 1 knows 0
+      [1, 1, 0, 1],        # 2 knows 0, 1 and 3
+      [1, 1, 0, 0],        # 3 knows 0 and 1
+   ],
+   # Expected output: -1
+);
 
 --------------------------------------------------------------------------------------------------------------
 PROBLEM NOTES:
@@ -92,27 +94,101 @@ Output is to STDOUT and will be each input followed by the corresponding output.
 # ------------------------------------------------------------------------------------------------------------
 # PRAGMAS, MODULES, AND SUBS:
 
-use v5.36;
-use utf8::all;
+   use v5.36;
+   use utf8::all;
+   use List::Util 'sum0';
 
-#
-sub asdf ($x, $y) {
-   -2.73*$x + 6.83*$y;
-}
+   # Find celebrities:
+   sub Celebrities ( $mref ) {
+      my $m = scalar(@$mref);
+      for my $i (0..$m-1) {
+         if ($m != scalar(@{$$mref[$i]})) {
+            return "Error: Matrix not square.";
+         }
+      }
+      my @out;
+      for my $i (0..$m-1) {
+         if (0 == sum0(@{$$mref[$i]})) {
+            if ($m-1 == sum0(map {$mref->[$_]->[$i]} 0..$m-1)) {
+               push @out, $i;
+            }
+         }
+      }
+      if (0 == scalar(@out)) {push @out, -1}
+      @out;
+   }
 
 # ------------------------------------------------------------------------------------------------------------
 # INPUTS:
-my @arrays = @ARGV ? eval($ARGV[0]) : ([2.61,-8.43],[6.32,84.98]);
+my @matrices = @ARGV ? eval($ARGV[0]) :
+(
+   # Example 1 input:
+   [
+      [0, 0, 0, 0, 1, 0],  # 0 knows 4
+      [0, 0, 0, 0, 1, 0],  # 1 knows 4
+      [0, 0, 0, 0, 1, 0],  # 2 knows 4
+      [0, 0, 0, 0, 1, 0],  # 3 knows 4
+      [0, 0, 0, 0, 0, 0],  # 4 knows NOBODY
+      [0, 0, 0, 0, 1, 0],  # 5 knows 4
+   ],
+   # Expected output: 4
+
+   # Example 2 input:
+   [
+      [0, 1, 0, 0],        # 0 knows 1
+      [0, 0, 1, 0],        # 1 knows 2
+      [0, 0, 0, 1],        # 2 knows 3
+      [1, 0, 0, 0],        # 3 knows 0
+   ],
+   # Expected output: -1
+
+   # Example 3 input:
+   [
+      [0, 0, 0, 0, 0],     # 0 knows NOBODY
+      [1, 0, 0, 0, 0],     # 1 knows 0
+      [1, 0, 0, 0, 0],     # 2 knows 0
+      [1, 0, 0, 0, 0],     # 3 knows 0
+      [1, 0, 0, 0, 0],     # 4 knows 0
+   ],
+   # Expected output: 0
+
+   # Example 4 input:
+   [
+      [0, 1, 0, 1, 0, 1],  # 0 knows 1, 3, 5
+      [1, 0, 1, 1, 0, 0],  # 1 knows 0, 2, 3
+      [0, 0, 0, 1, 1, 0],  # 2 knows 3, 4
+      [0, 0, 0, 0, 0, 0],  # 3 knows NOBODY
+      [0, 1, 0, 1, 0, 0],  # 4 knows 1, 3
+      [1, 0, 1, 1, 0, 0],  # 5 knows 0, 2, 3
+   ],
+   # Expected output: 3
+
+   # Example 5 input:
+   [
+      [0, 1, 1, 0],        # 0 knows 1 and 2
+      [1, 0, 1, 0],        # 1 knows 0 and 2
+      [0, 0, 0, 0],        # 2 knows NOBODY
+      [0, 0, 0, 0],        # 3 knows NOBODY
+   ],
+   # Expected output: -1
+
+   # Example 6 input:
+   [
+      [0, 0, 1, 1],        # 0 knows 2 and 3
+      [1, 0, 0, 0],        # 1 knows 0
+      [1, 1, 0, 1],        # 2 knows 0, 1 and 3
+      [1, 1, 0, 0],        # 3 knows 0 and 1
+   ],
+   # Expected output: -1
+);
 
 # ------------------------------------------------------------------------------------------------------------
 # MAIN BODY OF PROGRAM:
 $"=', ';
-for my $aref (@arrays) {
+for my $mref (@matrices) {
    say '';
-   my $x = $aref->[0];
-   my $y = $aref->[1];
-   my $z = asdf($x, $y);
-   say "x = $x";
-   say "y = $y";
-   say "z = $z";
+   say "Matrix = ";
+   say "[@$_]" for @$mref;
+   my @c = Celebrities($mref);
+   say "Celebrities = (@c)";
 }
