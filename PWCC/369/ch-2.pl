5ed3312502e2213f83bd6bbfac1cc1261ef32369
robbie-hatley <Hatley.Software@gmail.com>
2026-04-14 09:37:10 -0700

    PWCC 369.

--- PWCC/369/ch-2.pl
+++ PWCC/369/ch-2.pl
@@ -5,51 +5,48 @@
 --------------------------------------------------------------------------------------------------------------
 TITLE AND ATTRIBUTION:
 Solution in Perl for The Weekly Challenge 369-2,
-written by Robbie Hatley on Mon Apr 13, 2026.
+written by Robbie Hatley on Tue Apr 14, 2026.
 
 --------------------------------------------------------------------------------------------------------------
 PROBLEM DESCRIPTION:
 Task 369-2: Group Division
 Submitted by: Mohammad Sajid Anwar
+You are given a string, group size and filler character. Write a
+script to divide the string into groups of given size. In the
+last group if the string doesn’t have enough characters
+remaining fill with the given filler character.
 
-You are given a string, group size and filler character.
-
-Write a script to divide the string into groups of given size. In the last group if the string doesn’t have enough characters remaining fill with the given filler character.
 Example 1
-
 Input: $str = "RakuPerl", $size = 4, $filler = "*"
 Output: ("Raku", "Perl")
 
 Example 2
-
 Input: $str = "Python", $size = 5, $filler = "0"
 Output: ("Pytho", "n0000")
 
 Example 3
-
 Input: $str = "12345", $size = 3, $filler = "x"
 Output: ("123", "45x")
 
 Example 4
-
 Input: $str = "HelloWorld", $size = 3, $filler = "_"
 Output: ("Hel", "loW", "orl", "d__")
 
 Example 5
-
 Input: $str = "AI", $size = 5, $filler = "!"
 Output: "AI!!!"
 
 --------------------------------------------------------------------------------------------------------------
 PROBLEM NOTES:
-To solve this problem, ahtaht the elmu over the kuirens until the jibits koleit the smijkors.
+Chop and fill. The replacement version of Perl's "substr" function will be very useful here.
 
 --------------------------------------------------------------------------------------------------------------
 IO NOTES:
 Input is via either built-in variables or via @ARGV. If using @ARGV, provide one argument which must be a
-single-quoted array of arrays of double-quoted strings, in proper Perl syntax, like so:
+single-quoted array of arrays, with each inner array considing of a double-quoted string, a small positive
+integer, and a double-quoted fill character, in proper Perl syntax, like so:
 
-./ch-2.pl '(["rat", "bat", "cat"],["pig", "cow", "horse"])'
+./ch-2.pl '(["pseudopseudohypoparathyroidism", 4, "~"],["Floccinaucinihilipilification", 5, "#"])'
 
 Output is to STDOUT and will be each input followed by the corresponding output.
 
@@ -58,7 +55,7 @@ Output is to STDOUT and will be each input followed by the corresponding output.
 # ------------------------------------------------------------------------------------------------------------
 # PRAGMAS, MODULES, AND SUBS:
 
-   use v5.42;
+   use v5.36;
    use utf8::all;
 
    # Divide a given string into groups of a given size
